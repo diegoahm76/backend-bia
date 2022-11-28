15bb3fa142703d6e7e23ad1e9332d530cf4b0e3d
jdrb2098 <juan.rondon.bonilla@gmail.com>
2022-11-28 16:32:57 -0500

    sataticfiles finders

--- backend/settings.py
+++ backend/settings.py
@@ -17,7 +17,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
