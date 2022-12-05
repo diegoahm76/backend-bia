194c3615fe952822389c272f9c56aaa7f657fae2
jdrb2098 <juan.rondon.bonilla@gmail.com>
2022-12-05 17:24:43 -0500

    cors config

--- backend/settings.py
+++ backend/settings.py
@@ -205,7 +205,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 CSRF_TRUSTED_ORIGINS = ['https://web-production-7eaf.up.railway.app',
                         'https://web-production-e470.up.railway.app',
-                        'https://web-production-08ae.up.railway.app/',]
+                        'https://web-production-08ae.up.railway.app',]
 CORS_ALLOW_ALL_ORIGINS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
