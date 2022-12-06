43e51f928de78dcb713d61796773450aa41a588b
kamilo arias <youtube_.1034@hotmail.com>
2022-12-06 09:57:25 -0500

    merge conflict

--- almacen/views/bienes_views.py
+++ almacen/views/bienes_views.py
@@ -397,7 +397,7 @@ class GetElementosByIdNodo(generics.ListAPIView):
             return Response({'success': False, 'detail': 'No se encontró ningún elemento con el parámetro ingresado'}, status=status.HTTP_404_NOT_FOUND)
         
 
-class GetList(generics.ListAPIView):
+class SearchArticulos(generics.ListAPIView):
     serializer_class=CatalogoBienesSerializer
     queryset=CatalogoBienes.objects.all()
     def get(self,request):
@@ -409,9 +409,10 @@ class GetList(generics.ListAPIView):
                 else:filter[key]=value
         filter['nro_elemento_bien']=None
         filter['nivel_jerarquico']=5    
-        bien=CatalogoBienes.objects.filter(**filter).values()
+        bien=CatalogoBienes.objects.filter(**filter)
+        serializador=self.serializer_class(bien,many=True)
         if bien:
-            return Response({'success':True,'detail':'se encontró elementos','data':bien},status=status.HTTP_200_OK)
+            return Response({'success':True,'detail':'se encontró elementos','data':serializador.data},status=status.HTTP_200_OK)
         return Response({'success':True,'detail':'no se econtrò elementos','data':bien},status=status.HTTP_404_NOT_FOUND)
     
     
