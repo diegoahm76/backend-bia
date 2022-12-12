a3d83ec9b4d2e62b9fb69df22544464f255cef30
Oscar Rodriguez <oscarstep.123@gmail.com>
2022-12-12 11:44:31 -0500

    signal changes cargos personas

--- seguridad/signals/personas_signals.py
+++ seguridad/signals/personas_signals.py
@@ -1,6 +1,7 @@
-from django.db.models.signals import post_save, pre_delete,pre_save
+from django.db.models.signals import pre_save
+from django.db.models import Q
 from django.dispatch import receiver
-from seguridad.models import Personas, HistoricoEmails, HistoricoDireccion
+from seguridad.models import Personas, HistoricoEmails, HistoricoDireccion, Cargos
 
 @receiver(pre_save, sender=Personas)
 def create_historico_personas(sender, instance, **kwargs):
@@ -18,6 +19,35 @@ def create_historico_personas(sender, instance, **kwargs):
                     email_notificacion = current.email
                 )
                 historico_email.save()
+                
+            # MODIFICA CARGO
+            if previous.id_cargo:
+                if not current.id_cargo:
+                    persona_cargo = Personas.objects.filter(Q(id_cargo=previous.id_cargo.id_cargo) & ~Q(id_persona=previous.id_persona)).first()
+                    cargo = Cargos.objects.filter(id_cargo=previous.id_cargo.id_cargo).first()
+                    if not persona_cargo:
+                        cargo.item_usado=False
+                        cargo.save()
+                else:
+                    if previous.id_cargo.id_cargo != current.id_cargo.id_cargo:
+                        persona_cargo_current = Personas.objects.filter(id_cargo=current.id_cargo.id_cargo).first()
+                        persona_cargo_previous = Personas.objects.filter(Q(id_cargo=previous.id_cargo.id_cargo) & ~Q(id_persona=previous.id_persona)).first()
+                        cargo_current = Cargos.objects.filter(id_cargo=current.id_cargo.id_cargo).first()
+                        cargo_previous = Cargos.objects.filter(id_cargo=previous.id_cargo.id_cargo).first()
+                        if not persona_cargo_current:
+                            cargo_current.item_usado=True
+                            cargo_current.save()
+                        if not persona_cargo_previous:
+                            cargo_previous.item_usado=False
+                            cargo_previous.save()
+            else:
+                if current.id_cargo:
+                    persona_cargo = Personas.objects.filter(id_cargo=current.id_cargo.id_cargo).first()
+                    cargo = Cargos.objects.filter(id_cargo=current.id_cargo.id_cargo).first()
+                    if not persona_cargo:
+                        cargo.item_usado=True
+                        cargo.save()
+                
             
             # MODIFICA DIRECCION
             if previous.direccion_laboral!= current.direccion_laboral:
@@ -73,4 +103,3 @@ def create_historico_personas(sender, instance, **kwargs):
                     cod_pais_exterior = cod_pais_exterior
                 )
                 historico_direccion.save()
-                
