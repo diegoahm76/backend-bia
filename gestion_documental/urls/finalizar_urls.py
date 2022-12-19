567223ccde4b1854750dd78f9f8983a99c6e2fdf
kamilo arias <youtube_.1034@hotmail.com>
2022-12-19 17:23:08 -0500

    refactorización url finalizar_urls

--- gestion_documental/urls/finalizar_urls.py
+++ gestion_documental/urls/finalizar_urls.py
@@ -4,7 +4,7 @@ from gestion_documental.views import finalizar_views as views
 urlpatterns = [
     # FINALIZAR ROGANIGRAMA, CCD Y TRD
     path('',views.Activar.as_view(),name='activar'),
-    path('get-ccd-terminados/<str:id_organigrama>/', views.GetCCDTerminadoByPk.as_view(), name='ccd-terminados-get-by-id'),
-    path('get-trd-terminados/<str:id_ccd>/', views.GetTRDTerminadoByPk.as_view(), name='trd-terminados-get-by-id'),
-    path('get-tca-terminados/<str:id_ccd>/', views.GetTCATerminadoByPk.as_view(), name='tca-terminados-get-by-id')
+    path('get-ccd-terminados-by-org/<str:id_organigrama>/', views.GetCCDTerminadoByORG.as_view(), name='ccd-terminados-get-by-org'),
+    path('get-trd-terminados-by-ccd/<str:id_ccd>/', views.GetTRDTerminadoByCCD.as_view(), name='trd-terminados-get-by-ccd'),
+    path('get-tca-terminados-by-ccd/<str:id_ccd>/', views.GetTCATerminadoByCCD.as_view(), name='tca-terminados-get-by-ccd')
 ]
