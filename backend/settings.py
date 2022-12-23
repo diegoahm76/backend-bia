2c707b20d85de5afe71abfecb3b659ab606e3f35
jdrb2098 <juan.rondon.bonilla@gmail.com>
2022-12-23 11:28:10 -0500

    cors}

--- backend/settings.py
+++ backend/settings.py
@@ -102,12 +102,13 @@ SIMPLE_JWT = {
 
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    
     'whitenoise.middleware.WhiteNoiseMiddleware',
 
 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
