169d87cfe5f6914801ded0e233cb276c321ba5ca
Oscar Rodriguez <oscarstep.123@gmail.com>
2023-01-18 08:41:57 -0500

    util get cantidad posible

--- almacen/utils.py
+++ almacen/utils.py
@@ -82,25 +82,45 @@ class UtilAlmacen:
             
         return cantidades_disponibles
     
-    # @staticmethod
-    # def get_cantidad_disponible_despacho(id_bien, id_bodega, fecha_despacho):
-    #     inventario = Inventario.objects.filter(id_bien=id_bien, id_bodega=id_bodega).first()
-    #     saldo_actual = inventario.cantidad_entrante_consumo - (inventario.cantidad_saliente_consumo if inventario.cantidad_saliente_consumo else 0)
+    @staticmethod
+    def get_cantidad_posible(id_bien, id_bodega, id_despacho):
+        inventario = Inventario.objects.filter(id_bien=id_bien, id_bodega=id_bodega).first()
+        saldo_actual = inventario.cantidad_entrante_consumo - (inventario.cantidad_saliente_consumo if inventario.cantidad_saliente_consumo else 0)
         
-    #     entradas = EntradasAlmacen.objects.filter(fecha_entrada__gte=fecha_despacho, entrada_anulada=None)
-    #     cantidad_disponible = saldo_actual
+        despacho = DespachoConsumo.objects.filter(id_despacho_consumo=id_despacho).first()
         
-    #     if entradas:
-    #         entradas_id = [entrada.id_entrada_almacen for entrada in entradas] if entradas else []
-    #         cantidad_total_entradas = ItemEntradaAlmacen.objects.filter(id_entrada_almacen__in=entradas_id, id_bien=id_bien, id_bodega=id_bodega).aggregate(cantidad=Sum('cantidad'))
+        entradas = EntradasAlmacen.objects.filter(fecha_entrada__gte=despacho.fecha_despacho, entrada_anulada=None)
+        cantidad_disponible = saldo_actual
+        cantidad_total_entradas = 0
+        cantidad_posible = 0
+        
+        # HALLAR CANTIDAD DISPONIBLE
+        
+        if entradas:
+            entradas_id = [entrada.id_entrada_almacen for entrada in entradas] if entradas else []
+            cantidad_total_entradas = ItemEntradaAlmacen.objects.filter(id_entrada_almacen__in=entradas_id, id_bien=id_bien, id_bodega=id_bodega).aggregate(cantidad=Sum('cantidad'))
+            cantidad_total_entradas = cantidad_total_entradas['cantidad'] if cantidad_total_entradas else 0
             
-    #         cantidad_disponible = saldo_actual - cantidad_total_entradas['cantidad']
+            cantidad_disponible = saldo_actual - (cantidad_total_entradas if cantidad_total_entradas else 0)
         
-    #     despachos = DespachoConsumo.objects.filter(fecha_despacho__gte=fecha_despacho, despacho_anulado=None)
-    #     if despachos:
-    #         despachos_id = [despacho.id_despacho_consumo for despacho in despachos] if despachos else []
-    #         cantidad_total_despachos = ItemDespachoConsumo.objects.filter(id_despacho_consumo__in=despachos_id, id_bien=id_bien, id_bodega=id_bodega)
-    #         items_despachos_cantidad = [item_despacho.cantidad_despachada for item_despacho in items_despachos] if items_despachos else []
-    #         cantidad_total_despachos = sum(items_despachos_cantidad) if items_despachos_cantidad else 0
+        despachos = DespachoConsumo.objects.filter(fecha_despacho__gte=despacho.fecha_despacho, despacho_anulado=None)
         
-    #     return cantidad_disponible
+        if despachos:
+            despachos_id = [despacho.id_despacho_consumo for despacho in despachos] if despachos else []
+            
+            cantidad_total_despachos = ItemDespachoConsumo.objects.filter(id_despacho_consumo__in=despachos_id, id_bien_despachado=id_bien, id_bodega=id_bodega).aggregate(cantidad=Sum('cantidad_despachada'))
+            
+            cantidad_disponible = cantidad_disponible + (cantidad_total_despachos['cantidad'] if cantidad_total_despachos and cantidad_total_despachos['cantidad'] else 0)
+            
+            # HALLAR CANTIDAD POSIBLE
+            
+            despachos_id.remove(despacho.id_despacho_consumo)
+            
+            if despachos_id:
+                cantidad_total_despachos_except = ItemDespachoConsumo.objects.filter(id_despacho_consumo__in=despachos_id, id_bien_despachado=id_bien, id_bodega=id_bodega).aggregate(cantidad=Sum('cantidad_despachada'))
+                
+                cantidad_posible = cantidad_disponible - (cantidad_total_despachos_except['cantidad'] if cantidad_total_despachos_except['cantidad'] else 0) + cantidad_total_entradas
+            else:
+                cantidad_posible = cantidad_disponible
+            
+        return cantidad_posible
