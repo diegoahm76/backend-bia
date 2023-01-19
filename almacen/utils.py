6aefc0be5a229d0ac324e96c795999c93036cb5e
Oscar Rodriguez <oscarstep.123@gmail.com>
2023-01-19 14:14:11 -0500

    util cantidad por distribuir

--- almacen/utils.py
+++ almacen/utils.py
@@ -9,7 +9,8 @@ from almacen.models.inventario_models import (
 from almacen.models.bienes_models import EntradasAlmacen, ItemEntradaAlmacen
 from almacen.models.solicitudes_models import DespachoConsumo, ItemDespachoConsumo
 import re, requests
-from django.db.models import Q
+from django.db.models import Q, F
+from datetime import datetime, date,timedelta
 
 class UtilAlmacen:
     
@@ -59,9 +60,9 @@ class UtilAlmacen:
             cantidad_total_entradas = [{'id_bien': inventario.id_bien.id_bien, 'id_bodega': inventario.id_bodega.id_bodega, 'cantidad_disponible': saldo_actual}]
             if entradas:
                 entradas_id = [entrada.id_entrada_almacen for entrada in entradas] if entradas else []
-                cantidad_total_entradas = ItemEntradaAlmacen.objects.filter(id_entrada_almacen__in=entradas_id, id_bien=inventario.id_bien, id_bodega = inventario.id_bodega).values('id_bien', 'id_bodega').annotate(cantidad_disponible=saldo_actual - Sum('cantidad'))
-            
-            cantidades_disponibles.append(cantidad_total_entradas[0])   
+                cantidad_total_entradas = ItemEntradaAlmacen.objects.filter(id_entrada_almacen__in=entradas_id, id_bien=inventario.id_bien, id_bodega = inventario.id_bodega).values('id_bien', 'id_bodega').annotate(cantidad_disponible=saldo_actual - Sum('cantidad'))  
+            cantidades_disponibles.append(cantidad_total_entradas[0])
+        
         return cantidades_disponibles
 
     @staticmethod
@@ -123,3 +124,27 @@ class UtilAlmacen:
                 cantidad_posible = cantidad_disponible
             
         return cantidad_posible
+
+    @staticmethod
+    def get_cantidad_por_distribuir(id_bien, id_entrada, fecha_despacho):
+        fecha_hace_un_año=datetime.today()-timedelta(days=365)
+        cantidad_por_distribuir = 0
+        
+        item_entrada=ItemEntradaAlmacen.objects.filter(
+            id_entrada_almacen=id_entrada,
+            id_bien=id_bien,id_entrada_almacen__fecha_entrada__gte=fecha_hace_un_año,
+            id_entrada_almacen__fecha_entrada__lte=fecha_despacho).filter(
+                Q(id_entrada_almacen__id_tipo_entrada=2) | 
+                Q(id_entrada_almacen__id_tipo_entrada=3) | 
+                Q(id_entrada_almacen__id_tipo_entrada=4)).values(
+                    'id_bien',"id_entrada_almacen",
+                    codigo_bien=F('id_bien__codigo_bien'),
+                    nombre=F('id_bien__nombre'),
+                    numero_documento=F('id_entrada_almacen__numero_entrada_almacen'),
+                    tipo_documento=F('id_entrada_almacen__id_tipo_entrada__nombre')).annotate(cantidad_total_entrada=Sum('cantidad'))
+        
+        item_despachado=ItemDespachoConsumo.objects.filter(id_bien_despachado=id_bien,id_entrada_almacen_bien=id_entrada).values('id_bien_despachado','id_entrada_almacen_bien').annotate(cantidad_total_despachada=Sum('cantidad_despachada'))
+        
+        cantidad_por_distribuir=item_entrada[0]['cantidad_total_entrada'] - item_despachado[0]['cantidad_total_despachada'] if item_despachado and item_despachado[0]['cantidad_total_despachada'] else item_entrada[0]['cantidad_total_entrada']
+        
+        return cantidad_por_distribuir
