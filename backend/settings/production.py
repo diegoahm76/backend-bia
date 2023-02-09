a4675808bd5fbf0fdab9687940f274cc220076db
Oscar Rodriguez <oscarstep.123@gmail.com>
2023-02-09 11:10:06 -0500

    change debug property in beta and production

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -20,6 +20,3 @@ DATABASES = {
         'ATOMIC_REQUESTS': True
     }
 }
-
-if os.getcwd() == '/app':
-    DEBUG = False
