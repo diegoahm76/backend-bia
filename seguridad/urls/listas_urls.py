bda52e6bf444cbdde8c3180e47d91db159b9d922
Alejandro Sastoque <sastoque42@gmail.com>
2023-03-16 12:07:42 -0500

    Tipo Documentos, paises

--- seguridad/urls/listas_urls.py
+++ seguridad/urls/listas_urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('departamentos/', views.GetListDepartamentos.as_view(), name='get-list-departamentos'),
     path('municipios/', views.GetListMunicipios.as_view(), name='get-list-municipios'),
     path('tipo-persona/', views.GetListTipoPersona.as_view(), name='get-list-tipo-persona'),
+    path('paises/', views.GetListPaises.as_view(), name='get-list-paises'),
+    path('tipo-documento/', views.GetListTipoDocumento.as_view(), name='get-list-tipo-documento'),
 ]
