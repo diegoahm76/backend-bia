47008a61550eb964614ea49b464ead004c89abaa
Alejandro Sastoque <sastoque42@gmail.com>
2023-04-03 16:43:05 -0500

    Limpieza servicios

--- estaciones/serializers/datos_serializers.py
+++ estaciones/serializers/datos_serializers.py
@@ -1,32 +1,8 @@
-from estaciones.models.estaciones_models import Datos, DatosGuamal, DatosOcoa, DatosGuayuriba, DatosGaitan
+from estaciones.models.estaciones_models import Datos
 from rest_framework import serializers
 
 class DatosSerializer(serializers.ModelSerializer):
 
     class Meta:
         model=Datos
-        fields='__all__'
-
-class DatosSerializerGuamal(serializers.ModelSerializer):
-
-    class Meta:
-        model=DatosGuamal
-        fields='__all__'
-
-class DatosSerializerOcoa(serializers.ModelSerializer):
-
-    class Meta:
-        model=DatosOcoa
-        fields='__all__'
-
-class DatosSerializerGuayutiba(serializers.ModelSerializer):
-
-    class Meta:
-        model=DatosGuayuriba
-        fields='__all__'
-
-class DatosSerializerGaitan(serializers.ModelSerializer):
-
-    class Meta:
-        model=DatosGaitan
         fields='__all__'
