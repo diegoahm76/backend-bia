0f2b67248ece530988ef53f76af88cf493be4f19
Alejandro Sastoque <sastoque42@gmail.com>
2023-04-11 18:07:43 -0500

    Se realiza servicio que lista los datos de las estaciones migradas por id, por id-mes y por id fecha desde y fecha hasta

--- estaciones/urls/migracion_estaciones_urls.py
+++ estaciones/urls/migracion_estaciones_urls.py
@@ -3,8 +3,6 @@ from estaciones.views import migracion_estaciones_view as views
 
 urlpatterns = [
     # Historial Alertas
-    path('consultar-migracion-estaciones/',
-         views.ConsultarMigracion.as_view(), name='consultar-migracion-estaciones'),
     path('consultar-migracion-estaciones-id/<str:pk>/',
          views.ConsultarMigracionId.as_view(), name='consultar-migracion-estaciones-id'),
 ]
