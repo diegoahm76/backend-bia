fedbcc5ff7695250a2443e7ce74fb1376cb7987a
YeisonPyp <yeisonfabian_10@hotmail.com>
2023-05-05 08:32:20 -0500

    Arreglando errores, ajustando clases

--- recaudo/urls/pagos_urls.py
+++ recaudo/urls/pagos_urls.py
@@ -6,5 +6,11 @@ urlpatterns = [
     path('calidad-actuacion/<int:id>/', views.TipoActuacionView.as_view(), name='calidad-actuacion'),
     path('crear-facilidad-pago/', views.CrearFacilidadPagoView.as_view(), name='crear-facilidad-pago'),
     path('asignar-facilidad-pago/<int:id>/', views.FacilidadPagoUpdateView.as_view(), name='asignar-facilidad-pago'),
-    path('funcionarios/', views.FuncionariosView.as_view(), name='funcionarios')
-]
+    path('funcionarios/', views.FuncionariosView.as_view(), name='funcionarios'),
+    path('listado-obligaciones/', views.ListadoObligacionesViews.as_view(),name='listado-obligaciones'),
+    path('consulta-obligaciones/<int:id_obligaciones>/', views.ConsultaObligacionesViews.as_view(), name='consulta-obligaciones'),
+    path('consulta-deudores/', views.ConsultaDeudoresViews.as_view(),name='consulta-deudores'),
+    path('listado-facilidades-pagos/', views.ListadoFacilidadesPagoViews.as_view(),name='listado-facilidades-pagos'),
+    path('consulta-facilidades-pagos/<int:id>/', views.ConsultaFacilidadesPagosViews.as_view(), name='consulta-facilidades'),
+    ]
+    
