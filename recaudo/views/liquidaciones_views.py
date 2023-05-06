0ca21268cdabbd878fee3b6b23d06b5beeb5c181
Daniel <daniel.munoz.melendez@unillanos.edu.co>
2023-05-06 17:29:59 -0500

    Ajuste en servicio detalle de liquidación

--- recaudo/views/liquidaciones_views.py
+++ recaudo/views/liquidaciones_views.py
@@ -3,7 +3,8 @@ from recaudo.models.liquidaciones_models import (
     Deudores,
     LiquidacionesBase,
     DetalleLiquidacionBase,
-    Expedientes
+    Expedientes,
+    VariablesBase
 )
 from recaudo.serializers.liquidaciones_serializers import (
     OpcionesLiquidacionBaseSerializer,
@@ -12,7 +13,8 @@ from recaudo.serializers.liquidaciones_serializers import (
     LiquidacionesBasePostSerializer,
     DetallesLiquidacionBaseSerializer,
     DetallesLiquidacionBasePostSerializer,
-    ExpedientesSerializer
+    ExpedientesSerializer,
+    VariablesBaseSerializer
 )
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics, status
@@ -115,11 +117,16 @@ class DetallesLiquidacionBaseView(generics.GenericAPIView):
         return Response({'success': True, 'data': serializer.data}, status=status.HTTP_200_OK)
 
     def post(self, request):
-        serializer = DetallesLiquidacionBasePostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        variable_serializer = VariablesBaseSerializer(data=self.request.data.get('id_variable'))
+        if variable_serializer.is_valid(raise_exception=True):
+            variable = variable_serializer.save()
+            request.data['id_variable'] = variable.pk
+            serializer = DetallesLiquidacionBasePostSerializer(data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save(id_variable=variable)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(variable_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ExpedientesView(generics.ListAPIView):
