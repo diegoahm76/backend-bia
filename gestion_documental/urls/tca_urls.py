e681d5531efd6ab547c183d22c77d662290cf311
Oscar Rodriguez <oscarstep.123@gmail.com>
2023-05-09 18:01:16 -0500

    Changes in TCA and modulos_fixture

--- gestion_documental/urls/tca_urls.py
+++ gestion_documental/urls/tca_urls.py
@@ -2,22 +2,17 @@ from django.urls import path
 from gestion_documental.views import tca_views as views
 
 urlpatterns = [
-    path('unidades-by-ccd/get/<str:pk>/', views.GetUnidadesbyCCD.as_view(), name='unidades-get-by-ccd'),
-    path('cargos-by-unidades/get/', views.GetCargosByUnidades.as_view(), name='cargos-by-unidades-get'),
-    path('tca-list/get/', views.GetListTca.as_view(), name='tca-list-get'),
-    
     # Tabla de Control de Acceso
+    path('tca-list/get/', views.GetListTca.as_view(), name='tca-list-get'),
+    path('get-busqueda-tca/',views.BusquedaTCA.as_view(),name='get-tca'),
     path('create/', views.PostTablaControlAcceso.as_view(),name='create-tca'),
     path('update/<str:pk>/', views.UpdateTablaControlAcceso.as_view(), name='update-tca'),
     path('resume/<str:id_tca>/', views.ReanudarTablaControlAcceso.as_view(),name='resume-tca'),
     path('finish/<str:pk>/', views.FinalizarTablaControlAcceso.as_view(),name='finish-tca'),
-    path('serie-subserie-unidad-tca/clasificar/<str:id_tca>/',views.ClasifSerieSubserieUnidadTCA.as_view(),name='serie-subserie-unidad-tca-clasificar'),
-    path('serie-subserie-unidad-tca/update-clasif/<str:pk>/',views.UpdateClasifSerieSubserieUnidadTCA.as_view(),name='serie-subserie-unidad-tca-clasificar'),
-    path('serie-subserie-unidad-tca/get-clasif/<str:id_tca>/',views.GetClasifSerieSubserieUnidad.as_view(),name='serie-subserie-unidad-tca-clasificar'),
-    path('cargo-serie-subserie-unidad-tca/create/',views.asignar_cargo_unidad_permiso_expediente,name='cargo-serie-subserie-unidad-tca-crear'),
-    path('cargo-serie-subserie-unidad-tca/update/<str:pk>/',views.actualizar_cargo_unidad_permiso_expediente,name='cargo-serie-subserie-unidad-tca-update'),
-    path('cargo-serie-subserie-unidad-tca/delete/<str:id_trd>/',views.EliminarRelaciones.as_view(),name='cargo-serie-subserie-unidad-tca-delete'),
-    path('cargo-serie-subserie-unidad-tca/get/<str:id_tca>/',views.GetCargoUnidadPermisos.as_view(),name='cargo-serie-subserie-unidad-tca-get'),
-    path('get-busqueda-tca/',views.BusquedaTCA.as_view(),name='get-tca'),
-    path('get-series-subs-unidadorg-tca/<str:id_tca>/',views.GetSeriesSubSUnidadOrgTCA.as_view(),name='get-series-sub-unidad-tca'),
+    
+    # Catalogo TCA
+    path('catalogo-tca/clasificar/<str:id_tca>/',views.ClasifSerieSubserieUnidadTCA.as_view(),name='catalogo-tca-clasificar'),
+    path('catalogo-tca/update-clasif/<str:pk>/',views.UpdateClasifSerieSubserieUnidadTCA.as_view(),name='catalogo-tca-update-clasif'),
+    path('catalogo-tca/get-clasif/<str:id_tca>/',views.GetClasifExpedientesTCA.as_view(),name='catalogo-tca-get-clasif'),
+    path('catalogo-tca/delete-clasif/<str:id_tca>/',views.EliminarRelaciones.as_view(),name='catalogo-tca-delete-clasif'),
 ]
