0b3f28a3eb0746337f50f415c2aab5f98fe158b6
YeisonPyp <yeisonfabian_10@hotmail.com>
2023-05-11 17:09:11 -0500

    Reajuste de las excepciones de seguridad

--- seguridad/views/permisos_views.py
+++ seguridad/views/permisos_views.py
@@ -44,51 +44,6 @@ class PermisosModulosViewSet(viewsets.ModelViewSet):
     serializer_class = PermisosModuloPostSerializer
     permission_classes = [IsAuthenticated]
 
-#     def create(self, request, *args, **kwargs):
-#         serializer = self.get_serializer(data=request.data, many=isinstance(request.data,list))
-#         serializer.is_valid(raise_exception=True)
-#         self.perform_create(serializer)
-#         usuario = request.user.nombre_de_usuario
-#         print(usuario)
-#         user = User.objects.get(nombre_de_usuario = usuario)
-#         print(user)
-#         modulo = Modulos.objects.get(id_modulo = 2)
-#         permiso = Permisos.objects.get(cod_permiso = 'CR')
-#         direccion_ip = Util.get_client_ip(request)
-#         descripcion = []
-#         for i in request.data:
-#                 descripcion.append( "Usuario" + ":" + usuario + ";" + "Permisos(es):" + "=>")
-#             print(i)
-#             descripcion.append( "Modulo" + ":" + i["id_modulo"] + ";" + "Permiso" + ":" + i["cod_permiso"] )
-
-#         print(descripcion)
-#         Auditorias.objects.create(id_usuario = user, id_modulo = modulo, id_cod_permiso_accion = permiso, subsistema = "SEGU", dirip=direccion_ip, descripcion=descripcion, valores_actualizados='')
-#         headers = self.get_success_headers(serializer.data)
-#         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-
-# #------------------------------------------------> Borrar un permiso de un modulo
-# class DeletePermisoModulo(DestroyAPIView):
-#     serializer_class = PermisosModuloPostSerializer
-#     queryset = PermisosModulo.objects.all()
-
-#     def delete(self, request, pk):
-
-#         data = PermisosModulo.objects.get(id_permisos_modulo=pk)
-
-#         if data:
-#             data.delete()
-#             usuario = request.user.id_usuario
-#             user = User.objects.get(id_usuario = usuario)
-#             modulo = Modulos.objects.get(id_modulo = 2)
-#             permiso = Permisos.objects.get(cod_permiso = 'BO')
-#             direccion_ip = Util.get_client_ip(request)
-#             descripcion =   "Modulo:" + str(data.id_modulo.nombre_modulo) + ";" + "Permiso:" + str(data.cod_permiso.nombre_permiso) + "."
-#             print(descripcion)
-#             Auditorias.objects.create(id_usuario = user, id_modulo = modulo, id_cod_permiso_accion = permiso, subsistema = "SEGU", dirip=direccion_ip, descripcion=descripcion, valores_actualizados='')
-
-#             return Response({'detail':'El permiso fue eliminado del modulo'})
-#         else:
-#             return Response({'detail':'No existe el esa selección ingresada'})
 
 #====================================================>Vistas tabla PermisosModuloRol
 #----------------------------------------------------> Asignar un permiso de módulo a un rol
@@ -109,18 +64,12 @@ class UpdatePermisoModulo(RetrieveUpdateAPIView):
     serializer_class = PermisosModuloPostSerializer
     queryset = PermisosModulo.objects.all()
 
-# class ListarPermisosModulo(ListAPIView):
-#     serializer_class = PermisosModuloSerializer
-#     def get_queryset(self):
-#         return PermisosModulo.objects.all()
 
 class ListarPermisosModulo(ListAPIView):
     serializer_class = GetPermisosRolSerializer
     queryset = PermisosModuloRol.objects.all()
     
     def get(self, request):
-        # permisos_modulo_rol = self.queryset.all()
-        # modulos_list = [permiso_modulo_rol.id_permiso_modulo.id_modulo.id_modulo for permiso_modulo_rol in permisos_modulo_rol]
         modulos = Modulos.objects.all()
         serializer_modulos = ModulosRolSerializer(modulos, many=True)
         modulos_data = serializer_modulos.data
@@ -142,7 +91,7 @@ class ListarPermisosModulo(ListAPIView):
             }
             outputList.append(subsistema_data)
             
-        return Response({'success':True,'detail':'Se encontraron los siguientes permisos por módulo', 'data':outputList}, status=status.HTTP_200_OK)
+        return Response({'success':True, 'detail':'Se encontraron los siguientes permisos por módulo', 'data':outputList}, status=status.HTTP_200_OK)
 
 class DetailPermisosModulo(RetrieveAPIView):
     serializer_class = PermisosModuloSerializer
@@ -168,11 +117,11 @@ class UpdatePermisoModuloRol(RetrieveUpdateAPIView):
                         id_permiso_modulo = permiso_modulo_instance
                     )
                 else:
-                    return Response({'success': False, 'detail':'No existe uno de los permisos, verifique'}, status=status.HTTP_400_BAD_REQUEST)
+                    raise ValidationError('No existe uno de los permisos, verifique')
                 
-            return Response({'success': True, 'detail':'Se actualizaron los permisos del rol'}, status=status.HTTP_200_OK)     
+            return Response({'success':True, 'detail':'Se actualizaron los permisos del rol'}, status=status.HTTP_200_OK)     
         else:
-            return Response({'success': False, 'detail':'No se encontró el rol'}, status=status.HTTP_404_NOT_FOUND)
+            raise NotFound('No se encontró el rol')
 
 class ListarPermisosModuloRol(ListAPIView):
     serializer_class = PermisosModuloRolSerializer
@@ -219,7 +168,7 @@ class GetPermisosRolByRol(ListAPIView):
             }
             outputList.append(subsistema_data)
             
-        return Response({'success':True,'detail':'El rol ingresado tiene acceso a lo siguiente', 'data':outputList}, status=status.HTTP_200_OK)
+        return Response({'success':True, 'detail':'El rol ingresado tiene acceso a lo siguiente', 'data':outputList}, status=status.HTTP_200_OK)
 
 def util_prueba(id_usuario,tipo_entorno):
     
@@ -255,11 +204,8 @@ def util_prueba(id_usuario,tipo_entorno):
                 template = "ingreso-de-entorno.html"
                 
                 context = {'primer_nombre': usuario_instance.persona.primer_nombre}
-                #template = render_to_string((template), context)
-                #email_data = {'template': template, 'email_subject': subject, 'to_email': supersusuario.persona.email}
-                
+
                 Util.notificacion(supersusuario.persona,subject,template,nombre_de_usuario=usuario_instance.nombre_de_usuario)
-                #Util.send_email(email_data)
                 
                 raise PermissionDenied('NO SE LE PERMITIRÁ TRABAJAR EN ENTORNO LABORAL, dado que la persona no está actualmente vinculada o la fecha final del cargo ha vencido')
             
@@ -344,7 +290,7 @@ class GetEstructuraMenu(ListAPIView):
         tipo_entorno = request.query_params.get('tipo_entorno')
         
         if not id_usuario and not tipo_entorno:
-            return Response({'success':False, 'detail':'Debe enviar los parámetros de búsqueda'}, status=status.HTTP_400_BAD_REQUEST)
+            raise ValidationError('Debe enviar los parámetros de búsqueda')
         
         estructuras = EstructuraMenus.objects.order_by('subsistema', 'nivel_jerarquico', 'orden_por_padre')
         
@@ -375,7 +321,7 @@ class GetEstructuraMenu(ListAPIView):
             }
             outputList.append(subsistema_data)
         
-        return Response({'success':True,'detail':'Esta es la estructura de los menús actual','data':outputList}, status=status.HTTP_200_OK)   
+        return Response({'success':True, 'detail':'Esta es la estructura de los menús actual','data':outputList}, status=status.HTTP_200_OK)   
 
 class GetPermisosRolByEntorno(ListAPIView):
     serializer_class = GetPermisosRolSerializer
@@ -385,7 +331,7 @@ class GetPermisosRolByEntorno(ListAPIView):
         tipo_entorno = request.query_params.get('tipo_entorno')
         
         if not id_usuario and not tipo_entorno:
-            return Response({'success':False, 'detail':'Debe enviar los parámetros de búsqueda'}, status=status.HTTP_400_BAD_REQUEST)
+            raise ValidationError('Debe enviar los parámetros de búsqueda')
         
         usuario_instance = User.objects.filter(id_usuario=id_usuario).first()
         supersusuario = User.objects.filter(id_usuario=1).first()
@@ -395,18 +341,18 @@ class GetPermisosRolByEntorno(ListAPIView):
         roles_list = UsuariosRol.objects.filter(id_usuario=id_usuario)
         
         if not usuario_instance:
-            return Response({'success':False, 'detail':'Debe ingresar un usuario que exista'}, status=status.HTTP_400_BAD_REQUEST)
+            raise ValidationError('Debe ingresar un usuario que exista')
         else:
             if usuario_instance.is_superuser:
                 roles_list = roles_list.filter(id_rol=1)
                 tipo_entorno = None
             else:
                 if not tipo_entorno or tipo_entorno == '':
-                    return Response({'success':False, 'detail':'Debe indicar el tipo de entorno'}, status=status.HTTP_400_BAD_REQUEST)
+                    raise ValidationError('Debe indicar el tipo de entorno')
         
         if tipo_entorno:
             if tipo_entorno not in tipos_entorno:
-                return Response({'success':False, 'detail':'Debe ingresar un tipo de entorno valido'}, status=status.HTTP_400_BAD_REQUEST)
+                raise ValidationError('Debe ingresar un tipo de entorno valido')
         
             if tipo_entorno == 'C':
                 roles_list = roles_list.filter(id_rol=2)
@@ -420,7 +366,7 @@ class GetPermisosRolByEntorno(ListAPIView):
                     email_data = {'template': template, 'email_subject': subject, 'to_email': supersusuario.persona.email}
                     Util.send_email(email_data)
                     
-                    return Response({'success':False, 'permitido':False, 'detail':'NO SE LE PERMITIRÁ TRABAJAR EN ENTORNO LABORAL, dado que la persona no está actualmente vinculada o la fecha final del cargo ha vencido'}, status=status.HTTP_403_FORBIDDEN)
+                    raise PermissionDenied('NO SE LE PERMITIRÁ TRABAJAR EN ENTORNO LABORAL, dado que la persona no está actualmente vinculada o la fecha final del cargo ha vencido')
                 
                 roles_list = roles_list.exclude(id_rol=2)
         
@@ -449,7 +395,7 @@ class GetPermisosRolByEntorno(ListAPIView):
             }
             outputList.append(subsistema_data)
             
-        return Response({'success':True,'detail':'El tipo de entorno ingresado tiene acceso a lo siguiente', 'permitido':True, 'data':outputList}, status=status.HTTP_200_OK)   
+        return Response({'success':True, 'detail':'El tipo de entorno ingresado tiene acceso a lo siguiente', 'permitido':True, 'data':outputList}, status=status.HTTP_200_OK)   
 
 #----------------------------------------------------->Tabla Modulos
 
