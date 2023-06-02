043c0ce21dd8cb23d03b9ece0509ac41476b3956
YeisonPyp <yeisonfabian_10@hotmail.com>
2023-06-02 09:24:31 -0500

    merge develop

--- recaudo/models/cobros_models.py
+++ recaudo/models/cobros_models.py
@@ -9,12 +9,12 @@ class DocumentosCobro(models.Model):
     cod_deudor = models.ForeignKey(Deudores, on_delete=models.CASCADE, db_column='T405cod_deudor')
     fecha_cobro = models.DateTimeField(db_column='T405fecha_cobro')
     vencimiento = models.DateTimeField(db_column='T405vencimiento')
-    valor_deuda = models.IntegerField(default=0, db_column='T405valor_deuda')
+    valor_deuda = models.FloatField(default=0, db_column='T405valor_deuda')
     tasa_interes = models.ForeignKey(TasasInteres, on_delete=models.CASCADE, default=0, db_column='T405tasa_interes')
-    valor_mora = models.IntegerField(default=0, db_column='T405valor_mora')
-    porcentaje_descuento = models.IntegerField(default=0, db_column='T405porcentaje_descuento')
-    valor_descuento = models.IntegerField(default=0, db_column='T405valor_descuento')
-    valor_total = models.IntegerField(default=0, db_column='T405valor_total')
+    valor_mora = models.FloatField(default=0, db_column='T405valor_mora')
+    porcentaje_descuento = models.FloatField(default=0, db_column='T405porcentaje_descuento')
+    valor_descuento = models.FloatField(default=0, db_column='T405valor_descuento')
+    valor_total = models.FloatField(default=0, db_column='T405valor_total')
     estado = models.CharField(max_length=1, default='G', db_column='T405estado')
 
     class Meta:
@@ -71,7 +71,9 @@ class Cartera(models.Model):
     fin = models.DateField(null=True, blank=True,db_column='T417fin')
     id_rango = models.ForeignKey(RangosEdad, on_delete=models.CASCADE, db_column='T417id_rango')
     codigo_contable = models.ForeignKey(ConceptoContable, on_delete=models.CASCADE, db_column='T417codigo_contable')
-    fecha_facturacion = models.DateField(db_column='T417fecha_facturacion')
+    fecha_facturacion = models.DateField(null=True, blank=True, db_column='T417fecha_facturacion')
+    fecha_notificacion = models.DateField(null=True, blank=True, db_column='T417fecha_notificacion')
+    fecha_ejecutoriado = models.DateField(null=True, blank=True, db_column='T417fecha_ejecutoriado')
     numero_factura = models.CharField(max_length=255, db_column='T417numero_factura')
     monto_inicial = models.DecimalField(max_digits=30, decimal_places=2, db_column='T417monto_inicial')
 
