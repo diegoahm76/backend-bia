989362a64c6190f6e7ebea2b17a3a49a3d52979f
YeisonPyp <yeisonfabian_10@hotmail.com>
2023-06-09 10:28:19 -0500

    Cambios de Diana

--- recaudo/models/__init__.py
+++ recaudo/models/__init__.py
@@ -2,6 +2,5 @@ from .base_models import *
 from .cobros_models import *
 from .garantias_models import *
 from .liquidaciones_models import *
-from .notificaciones_models import *
 from .pagos_models import *
 from .procesos_models import *
