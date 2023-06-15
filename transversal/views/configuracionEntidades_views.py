daf15a7178a5d91b2bdac367b37fff15239bad2b
richardhbp99 <richardbejarano29@gmail.com>
2023-06-15 16:14:17 -0500

    agregar historial de configuraciones

--- transversal/views/configuracionEntidades_views.py
+++ transversal/views/configuracionEntidades_views.py
@@ -1,12 +1,15 @@
+from copy import deepcopy
 from django.forms import ValidationError
+from django.db.models import Max
+from django.utils import timezone
 from rest_framework import generics, status
 from rest_framework.exceptions import NotFound
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from transversal.serializers.entidades_serializers import ConfiguracionEntidadSerializer
-from transversal.models import ConfiguracionEntidad
-
+from transversal.models import ConfiguracionEntidad,HistoricoPerfilesEntidad
+from seguridad.models import Personas
 
 class GetConfiguracionEntidadByID(generics.GenericAPIView):
 
@@ -29,10 +32,154 @@ class UpdateConfiguracionEntidad(generics.UpdateAPIView):
     serializer_class = ConfiguracionEntidadSerializer
     queryset = ConfiguracionEntidad.objects.all()
     lookup_field = 'id_persona_entidad'
+    
+    def buscarConsecutivo(self,cod_perfil_histo):
+        max_valor = HistoricoPerfilesEntidad.objects.filter(cod_tipo_perfil_histo=cod_perfil_histo).aggregate(max_valor=Max('consec_asignacion_perfil_histo'))
+        print("dato maximo de "+str(cod_perfil_histo))
+        print(max_valor)
+        if not max_valor['max_valor']:
+            print("NO EXISTE REGISTRO")
+            return(1)
+        else:
 
+            print(max_valor['max_valor'])
+            return max_valor['max_valor']
+        
+
+    
+    def registrarHistoricoPerfilesEntidad(self,cod_tipo_perfil,consec,id_persona,fecha_inicio_periodo,observaciones):
+        
+        consecutivo=self.buscarConsecutivo(cod_tipo_perfil)
+        print(consecutivo)
+        historico_perfil = HistoricoPerfilesEntidad(
+                            id_persona_entidad_id=1,
+                            cod_tipo_perfil_histo=cod_tipo_perfil,
+                            consec_asignacion_perfil_histo=consecutivo+1,
+                            id_persona_perfil_histo_id=id_persona.id_persona,
+                            fecha_inicio_periodo=fecha_inicio_periodo,
+                            fecha_fin_periodo=timezone.now(),
+                            observaciones_cambio=observaciones,
+                            id_persona_cambia_id=1
+                            )
+        historico_perfil.save()
+
+        
+
+    
     def put(self, request, *args, **kwargs):
         instance = self.get_object()  # Obtener la instancia del objeto a actualizar
-        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        data_nueva = request.data
+        data_in = request.data
+
+        ##si el dato que cambia es el director
+        if instance.id_persona_director_actual:##si existe
+            
+            #si existe verifica que sea diferente
+            if int(instance.id_persona_director_actual.id_persona) != int(data_nueva['id_persona_director_actual']) :
+                #instance.fecha_inicio_dir_actual = timezone.now()
+                data_nueva['fecha_inicio_dir_actual']=timezone.now()
+                print(instance.fecha_inicio_dir_actual)
+
+                 #realiza el registro en historial
+                cod_tipo_perfil='Dire'   
+                consec=0
+                id_persona=instance.id_persona_director_actual
+                fecha_inicio_periodo=instance.fecha_inicio_dir_actual
+                observaciones=request.data.get('observaciones_de_cambio_director')
+                self.registrarHistoricoPerfilesEntidad(cod_tipo_perfil,consec,id_persona,fecha_inicio_periodo,observaciones)
+
+            else:#sino no no cambia la fecha
+                data_nueva['fecha_inicio_dir_actual']=instance.fecha_inicio_dir_actual
+        else:#sino existe lo crea 
+        
+                if data_nueva['id_persona_director_actual']:
+                    instance.fecha_inicio_dir_actual = timezone.now()
+                    data_nueva['fecha_inicio_dir_actual']=timezone.now()
+              
+        ##si es el coordinador de almacen
+        if instance.id_persona_coord_almacen_actual:##si existe
+            
+            #si existe verifica que sea diferente
+            if int(instance.id_persona_coord_almacen_actual.id_persona) != int(data_nueva['id_persona_coord_almacen_actual']) :
+                #instance.fecha_inicio_coord_alm_actual = timezone.now()
+                data_nueva['fecha_inicio_coord_alm_actual']=timezone.now()
+                print(instance.fecha_inicio_coord_alm_actual)
+
+                 #realiza el registro en historial
+                cod_tipo_perfil='CAlm'   
+                consec=0
+                id_persona=instance.id_persona_coord_almacen_actual
+                fecha_inicio_periodo=instance.fecha_inicio_coord_alm_actual
+                observaciones=request.data.get('observaciones_de_cambio_coord_almacen')
+                self.registrarHistoricoPerfilesEntidad(cod_tipo_perfil,consec,id_persona,fecha_inicio_periodo,observaciones)
+
+            else:#sino no no cambia la fecha
+                data_nueva['fecha_inicio_coord_alm_actual']=instance.fecha_inicio_coord_alm_actual
+        else:#sino existe lo crea 
+        
+                if data_nueva['id_persona_coord_almacen_actual']:
+                    instance.fecha_inicio_coord_alm_actual = timezone.now()
+                    data_nueva['fecha_inicio_coord_alm_actual']=timezone.now()
+        
+
+        #si es responsable de transporte
+        if instance.id_persona_respon_transporte_actual:##si existe
+            
+            #si existe verifica que sea diferente
+            if int(instance.id_persona_respon_transporte_actual.id_persona) != int(data_nueva['id_persona_respon_transporte_actual']) :
+                #instance.fecha_inicio_respon_trans_actual = timezone.now()
+                data_nueva['fecha_inicio_respon_trans_actual']=timezone.now()
+                print(instance.fecha_inicio_respon_trans_actual)
+
+                 #realiza el registro en historial
+                cod_tipo_perfil='RTra'   
+                consec=0
+                id_persona=instance.id_persona_respon_transporte_actual
+                fecha_inicio_periodo=instance.fecha_inicio_respon_trans_actual
+                observaciones=request.data.get('observaciones_de_cambio_respon_transporte')
+                self.registrarHistoricoPerfilesEntidad(cod_tipo_perfil,consec,id_persona,fecha_inicio_periodo,observaciones)
+
+            else:#sino no no cambia la fecha
+                data_nueva['fecha_inicio_respon_trans_actual']=instance.fecha_inicio_respon_trans_actual
+        else:#sino existe lo crea 
+        
+                if data_nueva['id_persona_respon_transporte_actual']:
+                    instance.fecha_inicio_respon_trans_actual = timezone.now()
+                    data_nueva['fecha_inicio_respon_trans_actual']=timezone.now()
+
+        #si el coordinador de vivero
+
+        if instance.id_persona_coord_viveros_actual:##si existe
+            
+            #si existe verifica que sea diferente
+            if int(instance.id_persona_coord_viveros_actual.id_persona) != int(data_nueva['id_persona_coord_viveros_actual']) :
+                #instance.fecha_inicio_coord_viv_actual = timezone.now()
+                data_nueva['fecha_inicio_coord_viv_actual']=timezone.now()
+                print(instance.fecha_inicio_coord_viv_actual)
+
+                 #realiza el registro en historial
+                cod_tipo_perfil='CViv'   
+                consec=0
+                id_persona=instance.id_persona_coord_viveros_actual
+                fecha_inicio_periodo=instance.fecha_inicio_coord_viv_actual
+                observaciones=request.data.get('observaciones_de_cambio_coord_viveros')
+                self.registrarHistoricoPerfilesEntidad(cod_tipo_perfil,consec,id_persona,fecha_inicio_periodo,observaciones)
+
+            else:#sino no no cambia la fecha
+                data_nueva['fecha_inicio_coord_viv_actual']=instance.fecha_inicio_coord_viv_actual
+        else:#sino existe lo crea 
+        
+                if data_nueva['id_persona_coord_viveros_actual']:
+                    instance.fecha_inicio_coord_viv_actual = timezone.now()
+                    data_nueva['fecha_inicio_coord_viv_actual']=timezone.now()
+
+
+        serializer = self.get_serializer(instance, data=data_nueva, partial=True)
         serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
+        self.perform_update(serializer)  # Guardar los cambios
+
+        
+
         return Response(serializer.data)
+
+    
