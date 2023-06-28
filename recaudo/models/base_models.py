3888732bddde031b7e031f0036163ac27d71d942
YeisonPyp <yeisonfabian_10@hotmail.com>
2023-06-28 14:15:19 -0500

    Modificacion en los nombres de las tablas de las base de datos

--- recaudo/models/base_models.py
+++ recaudo/models/base_models.py
@@ -2,77 +2,77 @@ from django.db import models
 
 
 class VariablesBase(models.Model):
-    id = models.BigAutoField(primary_key=True, db_column='T400id')
+    id = models.BigAutoField(primary_key=True, db_column='T400IdVariable')
     nombre = models.CharField(max_length=255, db_column='T400nombre')
     tipo = models.CharField(max_length=1, db_column='T400tipo')
-    valor_defecto = models.DecimalField(max_digits=30, decimal_places=2, default=0, db_column='T400valor_defecto')
+    valor_defecto = models.DecimalField(max_digits=30, decimal_places=2, default=0, db_column='T400valorDefecto')
     estado = models.IntegerField(default=1, db_column='T400estado')
 
     class Meta:
-        db_table = "T400variables_base"
+        db_table = "T400VariablesBase"
         verbose_name = 'Variable base'
         verbose_name_plural = 'Variables base'
 
 
 class NaturalezaJuridica(models.Model):
-    id = models.AutoField(primary_key=True, db_column='T408id')
+    id = models.AutoField(primary_key=True, db_column='T408IdNaturaleza')
     descripcion = models.CharField(max_length=255, db_column='T408descripcion')
 
     class Meta:
-        db_table = 'T408naturalezas_juridica'
+        db_table = 'T408NaturalezasJuridica'
         verbose_name = 'Naturaleza juridica'
         verbose_name_plural = 'Naturaleza juridica'
 
 
 class Ubicaciones(models.Model):
-    id = models.AutoField(primary_key=True, db_column='T409id')
+    id = models.AutoField(primary_key=True, db_column='T409IdUbicaciones')
     nombre = models.CharField(max_length=255, db_column='T409nombre')
     id_ubicacion_padre = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE, db_column='T409id_ubicacion_padre')
 
     class Meta:
-        db_table = 'T409ubicaciones'
+        db_table = 'T409Ubicaciones'
         verbose_name = 'Ubicación'
         verbose_name_plural = 'Ubicaciones'
 
 
 class RangosEdad(models.Model):
-    id = models.AutoField(primary_key=True, db_column='T411id')
+    id = models.AutoField(primary_key=True, db_column='T411IdRangosEdad')
     descripcion = models.CharField(max_length=255, db_column='T411descripcion')
     inicial = models.IntegerField(db_column='T411inicial')
     final = models.IntegerField(db_column='T411final')
 
     class Meta:
-        db_table = 'T411rangos_edad'
+        db_table = 'T411RangosEdad'
         verbose_name = 'Rango edad'
         verbose_name_plural = 'Rangos edad'
 
 
 class TiposBien(models.Model):
-    id = models.AutoField(primary_key=True, db_column='T415id')
+    id = models.AutoField(primary_key=True, db_column='T415IdTipoBien')
     descripcion = models.CharField(max_length=255, db_column='T415descripcion')
-    vigencia_avaluo = models.IntegerField(db_column='T415vigencia_avaluo')
+    vigencia_avaluo = models.IntegerField(db_column='T415vigenciaAvaluo')
 
     class Meta:
-        db_table = 'T415tipos_bien'
+        db_table = 'T415TiposBien'
         verbose_name = 'Tipo bien'
         verbose_name_plural = 'Tipos bien'
 
 
 class TiposPago(models.Model):
-    id = models.AutoField(primary_key=True, db_column='T412id')
+    id = models.AutoField(primary_key=True, db_column='T412IdTipoPago')
     descripcion = models.CharField(max_length=255, db_column='T412descripcion')
 
     class Meta:
-        db_table = 'T412tipos_pago'
+        db_table = 'T412TiposPago'
         verbose_name = 'Tipos pago'
         verbose_name_plural = 'Tipos pagos'
 
 
 class TipoActuacion(models.Model):
-    id = models.AutoField(primary_key=True, db_column='T427id')
+    id = models.AutoField(primary_key=True, db_column='T427IdTipoActuacion')
     descripcion = models.CharField(max_length=255, db_column='T427descripcion')
 
     class Meta:
-        db_table = 'T427tipo_actuacion'
+        db_table = 'T427TiposActuacion'
         verbose_name = 'Tipo actuación'
-        verbose_name_plural = 'Tipo actuación'
+        verbose_name_plural = 'Tipos actuación'
--- recaudo/models/liquidaciones_models.py
+++ recaudo/models/liquidaciones_models.py
@@ -3,25 +3,25 @@ from recaudo.models.base_models import NaturalezaJuridica, Ubicaciones
 
 
 class Deudores(models.Model):
-    codigo = models.AutoField(primary_key=True, db_column='T410codigo')
+    id = models.AutoField(primary_key=True, db_column='T410IdDeudor')
     identificacion = models.CharField(max_length=255, db_column='T410identificacion')
     nombres = models.CharField(max_length=255, db_column='T410nombres')
     apellidos = models.CharField(null=True, blank=True, max_length=255, db_column='T410apellidos')
     telefono = models.CharField(max_length=255, db_column='T410telefono')
     email = models.CharField(max_length=255, db_column='T410email')
-    ubicacion_id = models.ForeignKey(Ubicaciones, on_delete=models.CASCADE, db_column='T410ubicacion_id')
-    naturaleza_juridica_id = models.ForeignKey(NaturalezaJuridica, on_delete=models.CASCADE, db_column='T410naturaleza_juridica_id')
+    ubicacion_id = models.ForeignKey(Ubicaciones, on_delete=models.CASCADE, db_column='T410Id_Ubicacion')
+    naturaleza_juridica_id = models.ForeignKey(NaturalezaJuridica, on_delete=models.CASCADE, db_column='T410Id_NaturalezaJuridica')
 
     class Meta:
-        db_table = 'T410deudores'
+        db_table = 'T410Deudores'
         verbose_name = 'Deudor'
         verbose_name_plural = 'Deudores'
 
 
 class Expedientes(models.Model):
-    id = models.AutoField(primary_key=True, db_column='T407id')
-    codigo_expediente = models.CharField(max_length=255, db_column='T407codigo_expediente')
-    cod_deudor = models.ForeignKey(Deudores, on_delete=models.CASCADE, db_column='T407cod_deudor')
+    id = models.AutoField(primary_key=True, db_column='T407IdExpediente')
+    codigo_expediente = models.CharField(max_length=255, db_column='T407codigoExpediente')
+    id_deudor = models.ForeignKey(Deudores, on_delete=models.CASCADE, db_column='T407Id_Deudor')
     numero_resolucion = models.CharField(max_length=255, db_column='T407resolucion')
     cod_auto = models.CharField(max_length=255, db_column='T407auto')
     cod_recurso = models.CharField(max_length=255, db_column='T407recurso')
@@ -32,7 +32,7 @@ class Expedientes(models.Model):
 
 
 class OpcionesLiquidacionBase(models.Model):
-    id = models.BigAutoField(primary_key=True, db_column='T402id')
+    id = models.BigAutoField(primary_key=True, db_column='T402IdOpcionLiquidacionBase')
     nombre = models.CharField(max_length=255, db_column='T402nombre')
     estado = models.IntegerField(default=1, db_column='T402estado')
     version = models.IntegerField(default=1, db_column='T402version')
@@ -41,37 +41,37 @@ class OpcionesLiquidacionBase(models.Model):
     bloques = models.TextField(db_column='T402bloques')
 
     class Meta:
-        db_table = "T402opciones_liquidacion_base"
+        db_table = "T402OpcionesLiquidacionBase"
         verbose_name = 'Opcion liquidación base'
         verbose_name_plural = 'Opciones liquidación base'
 
 
 class LiquidacionesBase(models.Model):
-    id = models.AutoField(primary_key=True, db_column="T403id")
-    cod_deudor = models.ForeignKey(Deudores, on_delete=models.CASCADE, db_column="T403cod_deudor")
-    cod_expediente = models.ForeignKey(Expedientes, on_delete=models.CASCADE, db_column="T403cod_expediente")
-    fecha_liquidacion = models.DateTimeField(db_column="T403fecha_liquidacion")
+    id = models.AutoField(primary_key=True, db_column="T403IdLiquidacionBase")
+    id_deudor = models.ForeignKey(Deudores, on_delete=models.CASCADE, db_column="T403Id_Deudor")
+    id_expediente = models.ForeignKey(Expedientes, on_delete=models.CASCADE, db_column="T403Id_Expediente")
+    fecha_liquidacion = models.DateTimeField(db_column="T403fechaLiquidacion")
     vencimiento = models.DateTimeField(db_column="T403vencimiento")
-    periodo_liquidacion = models.CharField(max_length=255, db_column="T403periodo_liquidacion")
+    periodo_liquidacion = models.CharField(max_length=255, db_column="T403periodoLiquidacion")
     valor = models.IntegerField(default=0, db_column="T403valor")
     estado = models.CharField(max_length=1, default='G', db_column="T403estado")
 
     class Meta:
-        db_table = "T403liquidaciones_base"
+        db_table = "T403LiquidacionesBase"
         verbose_name = 'Liquidación base'
         verbose_name_plural = 'Liquidaciones base'
 
 
 class DetalleLiquidacionBase(models.Model):
-    id = models.AutoField(primary_key=True, db_column="T404id")
-    id_opcion_liq = models.ForeignKey(OpcionesLiquidacionBase, db_column="T403id_opcion_liq", on_delete=models.CASCADE)
-    id_liquidacion = models.ForeignKey(LiquidacionesBase, on_delete=models.CASCADE, db_column="T404id_liquidacion", related_name='detalles')
+    id = models.AutoField(primary_key=True, db_column="T404IdDetalle_LiquidacionBase")
+    id_opcion_liq = models.ForeignKey(OpcionesLiquidacionBase, db_column="T404Id_OpcionLiquidacionBase", on_delete=models.CASCADE)
+    id_liquidacion = models.ForeignKey(LiquidacionesBase, on_delete=models.CASCADE, db_column="T404Id_LiquidacionBase", related_name='detalles')
     variables = models.JSONField(db_column="T404variables")
     valor = models.IntegerField(default=0, db_column="T404valor")
     estado = models.IntegerField(default=1, db_column="T404estado")
     concepto = models.TextField(db_column="T404concepto")
 
     class Meta:
-        db_table = "T404detalles_liquidaciones_base"
+        db_table = "T404Detalles_LiquidacionesBase"
         verbose_name = 'Detalle liquidación base'
         verbose_name_plural = 'Detalles liquidación base'
