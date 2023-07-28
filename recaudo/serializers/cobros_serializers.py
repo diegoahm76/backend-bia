9341e7e2d189d1cc91966ba547eb1d3b4d440e92
Daniel <daniel.munoz.melendez@unillanos.edu.co>
2023-07-28 18:49:20 -0500

    Ajuste en serializer

--- recaudo/serializers/cobros_serializers.py
+++ recaudo/serializers/cobros_serializers.py
@@ -9,6 +9,9 @@ from recaudo.models.liquidaciones_models import (
 from recaudo.models.base_models import (
     RangosEdad
 )
+from recaudo.models.procesos_models import (
+    Procesos
+)
 
 
 class CarteraSerializer(serializers.ModelSerializer):
@@ -35,12 +38,24 @@ class ObligacionesSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class ProcesosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Procesos
+        fields = '__all__'
+
+
 class CarteraGeneralSerializer(serializers.ModelSerializer):
     id_deudor = DeudorSerializer(many=False)
     id_rango = RangosEdadSerializer(many=False)
     id_obligacion = ObligacionesSerializer(many=False)
+    proceso_cartera = serializers.SerializerMethodField()
 
     class Meta:
         model = Cartera
         fields = ('id', 'id_obligacion', 'dias_mora', 'valor_intereses', 'valor_sancion', 'inicio', 'fin', 'id_rango', 'codigo_contable', 'fecha_facturacion', 'fecha_notificacion',
                   'fecha_ejecutoriado', 'numero_factura', 'monto_inicial', 'tipo_cobro', 'id_deudor', 'proceso_cartera')
+
+    def get_proceso_cartera(self, obj):
+        procesos_cartera = obj.proceso_cartera.filter(fin__isnull=True)
+        serializer = ProcesosSerializer(instance=procesos_cartera, many=True)
+        return serializer.data
