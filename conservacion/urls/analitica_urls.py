ca1a9ac926c859ac593d3a6b29d50e9aa85e99e2
Oscar Rodriguez <oscarstep.123@gmail.com>
2023-08-02 07:54:31 -0500

    Feature: Services entrega 47

---
+++ conservacion/urls/analitica_urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from conservacion.views import analitica_views as views
+
+urlpatterns = [
+    # Tablero de Control
+    path('tablero-control/get/', views.TableroControlGetView.as_view(), name='tablero-control-get'),
+    path('busqueda-bienes-mezclas/get/', views.BienesMezclasFilterGetView.as_view(), name='busqueda-bienes-mezclas'),
+]
