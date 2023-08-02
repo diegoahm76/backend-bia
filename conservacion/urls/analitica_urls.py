a0a52d1455eaf21a2f39d1f41bf103d4229c0c0f
Oscar Rodriguez <oscarstep.123@gmail.com>
2023-08-02 13:42:32 -0500

    Feature: New service entrega 47 get ultimos movimientos

--- conservacion/urls/analitica_urls.py
+++ conservacion/urls/analitica_urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     # Tablero de Control
     path('tablero-control/get/', views.TableroControlGetView.as_view(), name='tablero-control-get'),
     path('busqueda-bienes-mezclas/get/', views.BienesMezclasFilterGetView.as_view(), name='busqueda-bienes-mezclas'),
+    path('historico-movimientos/get/', views.HistoricoMovimientosGetView.as_view(), name='historico-movimientos'),
 ]
