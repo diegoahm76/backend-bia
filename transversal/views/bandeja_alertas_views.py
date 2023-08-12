ed92ac597f12b1d0e92dcce0a582db6f50a740f2
richardhbp99 <richardbejarano29@gmail.com>
2023-08-12 09:24:09 -0500

    update de bandeja de alertas

--- transversal/views/bandeja_alertas_views.py
+++ transversal/views/bandeja_alertas_views.py
@@ -1,3 +1,4 @@
+import copy
 from rest_framework import generics
 from transversal.models.alertas_models import FechaClaseAlerta
 
@@ -45,3 +46,43 @@ class AlertasBandejaAlertaPersonaGetByBandeja(generics.ListAPIView):
         
         serializer = self.serializer_class(items,many=True)
         return Response({'success':True,'detail':"Se encontron los siguientes  registros.",'data':serializer.data},status=status.HTTP_200_OK)
+
+
+class AlertasBandejaAlertaPersonaUpdate(generics.UpdateAPIView):
+    serializer_class = AlertasBandejaAlertaPersonaPutSerializer
+    queryset = AlertasBandejaAlertaPersona.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        instance = self.get_object()
+        previus=copy.copy(instance)
+        data_in=request.data
+        # Verificar si la instancia existe
+        if not instance:
+            return Response({'detail': 'El item de bandeja  no existe.'}, status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            
+            serializer = self.serializer_class(instance, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            instance=serializer.save()
+            
+            if 'repeticiones_suspendidas' in data_in:
+                if previus.repeticiones_suspendidas != instance.repeticiones_suspendidas and instance.repeticiones_suspendidas==True:
+                    #print("PRE CONDICION")
+                    alerta_programada=AlertasProgramadas.objects.filter(id_alerta_programada=instance.id_alerta_generada.id_alerta_programada_origen).first()
+                    if not alerta_programada:
+                        raise NotFound('No existe alerta programada')
+                    #print("PRE CONDICION")
+                    if alerta_programada.agno_cumplimiento:
+                        print("CONDICION CON AÑO")
+                    else:
+                        print("SIN AÑO")
+
+
+        except ValidationError as e:       
+            raise ValidationError(e.detail)
+
+
+        return Response({'success': True, 'detail': 'Se actualizó la configuración de clase de alerta correctamente.', 'data': serializer.data}, status=status.HTTP_200_OK)
