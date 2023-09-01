6a5517d6944237024ebfa12afbe544c58b04dd56
richardhbp99 <richardbejarano29@gmail.com>
2023-09-01 17:02:20 -0500

    implementacion de creacion bandeja de alertas en administrador de personas

--- gestion_documental/serializers/radicados_consecutivos_serializers.py
+++ gestion_documental/serializers/radicados_consecutivos_serializers.py
@@ -19,6 +19,21 @@ class ConfigTiposRadicadoAgnoUpDateSerializer(serializers.ModelSerializer):
                     fields=['agno_radicado','prefijo_consecutivo'],
                     message='Ya existe una configuracion con este prefijo')
                     ]
+class ConfigTiposRadicadoAgnoCreateSerializer(serializers.ModelSerializer):
+  
+    class Meta:
+        model = ConfigTiposRadicadoAgno
+        fields = '__all__'
+        validators =[
+                    UniqueTogetherValidator(queryset=ConfigTiposRadicadoAgno.objects.all(),
+                    fields=['agno_radicado', 'cod_tipo_radicado'],
+                    message='Ya existe una configuracion para este radicado'),
+                    UniqueTogetherValidator(queryset=ConfigTiposRadicadoAgno.objects.all(),
+                    fields=['agno_radicado','prefijo_consecutivo'],
+                    message='Ya existe una configuracion con este prefijo')
+                    ]
+
+
 
 class ConfigTiposRadicadoAgnoGetSerializer(serializers.ModelSerializer):
     class Meta:
