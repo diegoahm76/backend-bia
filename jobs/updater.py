56f597ae9db203e80dd6e9783c6a99d0e2f5ae25
richardhbp99 <richardbejarano29@gmail.com>
2023-09-05 14:56:08 -0500

    validaciones y auditoria entrega 59

--- jobs/updater.py
+++ jobs/updater.py
@@ -1,10 +1,11 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
-from .jobs import generar_alerta
+from .jobs import *
 
 def start():
 	scheduler = BackgroundScheduler()
 	trigger = CronTrigger(hour=23, minute=59, second=0)
 	scheduler.add_job(generar_alerta, trigger=trigger)
-	#scheduler.add_job(generar_alerta, 'interval', seconds=10)
+	#scheduler.add_job(generar_conf_radicado_gest, 'interval', seconds=5)
 	scheduler.start()
+
