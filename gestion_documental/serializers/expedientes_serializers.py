18425801aa4c9be38db0d6f59ff7d28b7af40674
kevin <kevinandrescas98@gmail.com>
2023-09-09 12:14:10 -0500

    Update_Expedientes

--- gestion_documental/serializers/expedientes_serializers.py
+++ gestion_documental/serializers/expedientes_serializers.py
@@ -5,13 +5,33 @@ from rest_framework.validators import UniqueValidator, UniqueTogetherValidator
 from django.db.models import Max 
 
 from gestion_documental.models.expedientes_models import ExpedientesDocumentales,ArchivosDigitales,DocumentosDeArchivoExpediente,IndicesElectronicosExp,Docs_IndiceElectronicoExp,CierresReaperturasExpediente,ArchivosSoporte_CierreReapertura
+from gestion_documental.models.trd_models import TablaRetencionDocumental
 
 
 ######################### SERIALIZERS DEPOSITO #########################
 
 #Buscar-Expediente
 class ExpedienteSearchSerializer(serializers.ModelSerializer):
-    
+
+    nombre_serie_origen = serializers.ReadOnlyField(source='id_serie_origen.nombre', default=None)
+    nombre_subserie_origen = serializers.ReadOnlyField(source='id_subserie_origen.nombre', default=None)
+    nombre_unidad_org = serializers.ReadOnlyField(source='id_und_seccion_propietaria_serie.nombre', default=None)
+    nombre_trd_origen = serializers.ReadOnlyField(source='id_trd_origen.nombre', default=None)
+
     class Meta:
         model =  ExpedientesDocumentales
-        fields = '__all__'
+        fields = ['codigo_exp_und_serie_subserie','id_expediente_documental','titulo_expediente','id_und_seccion_propietaria_serie','nombre_unidad_org','id_serie_origen','nombre_serie_origen','id_subserie_origen','nombre_subserie_origen','nombre_subserie_origen','id_trd_origen','nombre_trd_origen','fecha_apertura_expediente']
+
+
+class ListarTRDSerializer(serializers.ModelSerializer):
+    nombre_tdr_origen = serializers.ReadOnlyField(source='id_trd_origen.nombre', default=None)
+    actual_tdr_origen = serializers.ReadOnlyField(source='id_trd_origen.actual', default=None)
+    fecha_retiro_produccion_tdr_origen = serializers.ReadOnlyField(source='id_trd_origen.fecha_retiro_produccion', default=None)
+    estado_actual = serializers.SerializerMethodField()  # Nuevo campo para el estado actual
+
+    class Meta:
+        model = ExpedientesDocumentales
+        fields = ['id_trd_origen', 'nombre_tdr_origen', 'actual_tdr_origen', 'fecha_retiro_produccion_tdr_origen', 'estado_actual']
+
+    def get_estado_actual(self, obj):
+        return "ACTUAL" if obj.id_trd_origen.actual else "NO ACTUAL"
--- gestion_documental/views/expedientes_views.py
+++ gestion_documental/views/expedientes_views.py
@@ -1,10 +1,11 @@
 from gestion_documental.models.expedientes_models import ExpedientesDocumentales,ArchivosDigitales,DocumentosDeArchivoExpediente,IndicesElectronicosExp,Docs_IndiceElectronicoExp,CierresReaperturasExpediente,ArchivosSoporte_CierreReapertura
 from rest_framework.exceptions import ValidationError,NotFound,PermissionDenied
 from django.shortcuts import get_object_or_404
+from gestion_documental.models.trd_models import TablaRetencionDocumental
 from seguridad.utils import Util
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from gestion_documental.serializers.expedientes_serializers import ExpedienteSearchSerializer
+from gestion_documental.serializers.expedientes_serializers import ExpedienteSearchSerializer, ListarTRDSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from django.db.models import Max 
@@ -24,9 +25,13 @@ class ExpedienteSearch(generics.ListAPIView):
         titulo_expediente = self.request.query_params.get('titulo_expediente', '').strip()
         codigo_exp_und_serie_subserie = self.request.query_params.get('codigo_exp_und_serie_subserie', '').strip()
         fecha_apertura_expediente = self.request.query_params.get('fecha_apertura_expediente', '').strip()
-        id_serie_origen = self.request.query_params.get('id_serie_origen', '').strip()
-        id_subserie_origen = self.request.query_params.get('id_subserie_origen', '').strip()
+        nombre_serie_origen = self.request.query_params.get('id_serie_origen', '').strip()
+        nombre_subserie_origen = self.request.query_params.get('id_subserie_origen', '').strip()
         palabras_clave_expediente = self.request.query_params.get('palabras_clave_expediente', '').strip()
+        codigos_uni_serie_subserie = self.request.query_params.get('codigos_uni_serie_subserie', '').strip()
+        trd_nombre = self.request.query_params.get('trd_nombre', '').strip()
+
+
 
         # Filtrar por atributos específicos referentes a un expediente (unión de parámetros)
         queryset = ExpedientesDocumentales.objects.filter(estado='A')  # Filtrar por estado 'A'
@@ -39,12 +44,37 @@ class ExpedienteSearch(generics.ListAPIView):
         if fecha_apertura_expediente:
             queryset = queryset.filter(fecha_apertura_expediente__icontains=fecha_apertura_expediente)
 
-        if id_serie_origen:
-            queryset = queryset.filter(id_serie_origen=id_serie_origen)
+        if nombre_serie_origen:
+            queryset = queryset.filter(id_serie_origen__nombre__icontains=nombre_serie_origen)
 
-        if id_subserie_origen:
-            queryset = queryset.filter(id_subserie_origen=id_subserie_origen)
+        if nombre_subserie_origen:
+            queryset = queryset.filter(id_subserie_origen__nombre__icontains=nombre_subserie_origen)
+   
+        if codigos_uni_serie_subserie:
+            queryset = queryset.filter(codigo_exp_und_serie_subserie__startswith=codigos_uni_serie_subserie)
 
+        if trd_nombre:
+            queries = []
+            
+            if trd_nombre.lower() == 'actual':
+                queries.append(queryset.filter(id_trd_origen__nombre__icontains=trd_nombre))
+                queries.append(queryset.filter(id_trd_origen__fecha_retiro_produccion__icontains=trd_nombre))
+                queries.append(queryset.filter(id_trd_origen__actual=True))
+            elif trd_nombre.lower() == 'no actual':
+                queries.append(queryset.filter(id_trd_origen__nombre__icontains=trd_nombre))
+                queries.append(queryset.filter(id_trd_origen__fecha_retiro_produccion__icontains=trd_nombre))
+                queries.append(queryset.filter(id_trd_origen__actual=False))
+            else:
+                queries.append(queryset.filter(id_trd_origen__nombre__icontains=trd_nombre))
+                queries.append(queryset.filter(id_trd_origen__fecha_retiro_produccion__icontains=trd_nombre))
+                queries.append(queryset.filter(id_trd_origen__actual__icontains=trd_nombre))
+            
+            # Combinamos todas las consultas utilizando comas
+            queryset = queries[0]
+            for query in queries[1:]:
+                queryset = queryset | query
+            
+            
         if palabras_clave_expediente:
             search_vector = SearchVector('palabras_clave_expediente')
             search_query = SearchQuery(palabras_clave_expediente)
@@ -73,3 +103,30 @@ class ExpedienteSearch(generics.ListAPIView):
             'detail': 'Se encontraron los siguientes registros.',
             'data': serializer.data
         }, status=status.HTTP_200_OK)
+
+
+
+#LISTAR_DATOS_TRD
+class TrdDateGet(generics.ListAPIView):
+    serializer_class = ListarTRDSerializer
+    queryset = ExpedientesDocumentales.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+
+        if not queryset.exists():
+            return Response({
+                'success': False,
+                'detail': 'No se encontraron datos de expedientes documentales registrados.',
+                'data': []
+            }, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.get_serializer(queryset, many=True)
+
+        return Response({
+            'success': True,
+            'detail': 'Se encontraron los siguientes resultados',
+            'data': serializer.data
+        })
