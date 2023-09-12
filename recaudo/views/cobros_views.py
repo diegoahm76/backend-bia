c4c89ab6e72c45206060f771ebc03cbd2d1d878e
YeisonPyp <yeisonfabian_10@hotmail.com>
2023-09-12 10:25:13 -0500

    Cambios para mostrar

--- recaudo/views/cobros_views.py
+++ recaudo/views/cobros_views.py
@@ -13,14 +13,14 @@ from rest_framework.pagination import PageNumberPagination
 class CarteraView(generics.ListAPIView):
     queryset = Cartera.objects.all()
     serializer_class = CarteraGeneralSerializer
-    pagination_class = PageNumberPagination
-    # permission_classes = [IsAuthenticated]
+    # pagination_class = PageNumberPagination
+    # # permission_classes = [IsAuthenticated]
 
     def get(self, request):
         queryset = self.get_queryset()
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.serializer_class(page, many=True)
-            return self.get_paginated_response({'success': True, 'data': serializer.data})
+        # page = self.paginate_queryset(queryset)
+        # if page is not None:
+        #     serializer = self.serializer_class(page, many=True)
+        #     return self.get_paginated_response({'success': True, 'data': serializer.data})
         serializer = self.serializer_class(queryset, many=True)
         return Response({'success': True, 'data': serializer.data}, status=status.HTTP_200_OK)
