1dce444ec210fd0802ebc4ca49c3c46d7a9766a8
kevin <kevinandrescas98@gmail.com>
2023-09-13 17:56:15 -0500

    Update_Metadatos

--- gestion_documental/urls/expedientes_url.py
+++ gestion_documental/urls/expedientes_url.py
@@ -7,6 +7,12 @@ urlpatterns = [
     #Cierre de expedientes         
      path('expedientes/buscar-expediente/',views.ExpedienteSearch.as_view(), name='buscar-expediente'),
      path('expedientes/listar-trd/',views.TrdDateGet.as_view(), name='listar-trd'),
+     path('expedientes/agregar-archivo-soporte/',views.AgregarArchivoSoporte.as_view(), name='listar-trd'),
+     path('expedientes/orden-siguiente/',views.ExpedienteGetOrden.as_view(), name='orden-expediente'),
+     path('expedientes/orden-actual/0',views.EstanteDepositoGetOrdenActual.as_view(), name='orden-expediente-actual'),
+
+
+
 
      
 ]
