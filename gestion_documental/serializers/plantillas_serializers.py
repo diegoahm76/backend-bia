5336411433f86fbd85a3021dc0518409534d9ba5
richardhbp99 <richardbejarano29@gmail.com>
2023-09-19 09:59:03 -0500

    actualizacio y eliminacion de plantilla

--- gestion_documental/serializers/plantillas_serializers.py
+++ gestion_documental/serializers/plantillas_serializers.py
@@ -10,6 +10,39 @@ class  PlantillasDocCreateSerializer(serializers.ModelSerializer):
         model =  PlantillasDoc
         fields = '__all__'
 
+class  PlantillasDocBusquedaAvanzadaSerializer(serializers.ModelSerializer):
+    nombre_tipologia=serializers.ReadOnlyField(source='id_tipologia_doc_trd.nombre',default=None)
+    ruta=serializers.ReadOnlyField(source='id_archivo_digital.ruta_archivo',default=None)
+    extension=serializers.ReadOnlyField(source='id_archivo_digital.formato',default=None)
+    class Meta:
+        model =  PlantillasDoc
+        fields = ['id_plantilla_doc','nombre','nombre_tipologia','ruta','extension']
+
+
+class  PlantillasDocGetSeriallizer(serializers.ModelSerializer):
+    nombre_creador=serializers.ReadOnlyField(source='id_persona_crea_plantilla.nombre',default=None)
+    nombre_completo = serializers.SerializerMethodField()
+    cod_tipo_acceso_display = serializers.CharField(source='get_cod_tipo_acceso_display', default=None)
+    class Meta:
+        model =  PlantillasDoc
+        fields = '__all__'
+
+    def get_nombre_completo(self, obj):
+            nombre_completo_responsable = None
+            nombre_list = [obj.id_persona_crea_plantilla.primer_nombre, obj.id_persona_crea_plantilla.segundo_nombre,
+                            obj.id_persona_crea_plantilla.primer_apellido, obj.id_persona_crea_plantilla.segundo_apellido]
+            nombre_completo_responsable = ' '.join(item for item in nombre_list if item is not None)
+            nombre_completo_responsable = nombre_completo_responsable if nombre_completo_responsable != "" else None
+            return nombre_completo_responsable
+
+
+
+
+
+class  PlantillasDocUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =  PlantillasDoc
+        fields = '__all__'
 class  TipologiasDocSerializerGetSerializer(serializers.ModelSerializer):
     class Meta:
         model =  TipologiasDoc
@@ -19,3 +52,22 @@ class AccesoUndsOrg_PlantillaDocCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model =  AccesoUndsOrg_PlantillaDoc
         fields = '__all__'
+
+class AccesoUndsOrg_PlantillaDocGetSerializer(serializers.ModelSerializer):
+    nombre_unidad=serializers.ReadOnlyField(source='id_und_organizacional.nombre',default=None)
+    class Meta:
+        model =  AccesoUndsOrg_PlantillaDoc
+        fields = '__all__'
+
+
+
+
+
+class PlantillasDocSerializer(serializers.ModelSerializer):
+    accesos_unidades_organizacionales = AccesoUndsOrg_PlantillaDocGetSerializer(many=True, read_only=True)
+    class Meta:
+        model = PlantillasDoc
+        fields = '__all__'
+
+
+
