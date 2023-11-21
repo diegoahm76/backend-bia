cfadaae9d8e9760203b0c7e74f2ff0125f8732b7
HectorStiven <vigoyahector90@gmail.com>
2023-11-21 10:12:10 -0500

    servicios get ne entrega de zonas hidrics terminados

--- recurso_hidrico/admin.py
+++ recurso_hidrico/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from recurso_hidrico.models.zonas_hidricas_models import SubZonaHidrica
+
 # Register your models here.
+
+admin.site.register(SubZonaHidrica)
--- recurso_hidrico/urls/zonas_hidricas_urls.py
+++ recurso_hidrico/urls/zonas_hidricas_urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from recurso_hidrico.views import zonas_hidricas_views as views
 
 urlpatterns = [
-    path('macro-cuencas/get/', views.MacroCuencasListView.as_view(), name='macro-cuencas'),
-    path('zona_hidrica/get/', views.ZonaHidricaListView.as_view(), name='zonas-hidricas'),
-    
-]
+        path('macro-cuencas/get/', views.MacroCuencasListView.as_view(), name='macro-cuencas'),
+        path('zona_hidrica/get/<str:pk>/', views.ZonaHidricaListView.as_view(), name='zonas-hidricas'),
+        path('tipozonahidrica/get/', views.TipoZonaHidricaListView.as_view(), name='macro-cuencas'),
+        path('subZonahidrica/get/<str:pk>/', views.SubZonaHidricaListView.as_view(), name='macro-cuencas'),
+    ]
