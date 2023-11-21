7f92522120658e28713860e77a15dcdac5b370eb
richardhbp99 <richardbejarano29@gmail.com>
2023-11-21 16:05:51 -0500

    entrega 98 arreglos en modelo de anexos

--- gestion_documental/admin.py
+++ gestion_documental/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from gestion_documental.models.radicados_models import PQRSDF, Anexos, Anexos_PQR, Estados_PQR, EstadosSolicitudes, T262Radicados
+from gestion_documental.models.radicados_models import PQRSDF, Anexos, Anexos_PQR, Estados_PQR, EstadosSolicitudes, MetadatosAnexosTmp, T262Radicados
 from gestion_documental.models.trd_models import (
     TablaRetencionDocumental,
     TiposMediosDocumentos,
@@ -63,3 +63,4 @@ admin.site.register(T262Radicados)
 admin.site.register(Estados_PQR)
 admin.site.register(Anexos_PQR)
 admin.site.register(Anexos)
+admin.site.register(MetadatosAnexosTmp)
