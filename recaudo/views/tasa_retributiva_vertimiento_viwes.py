e1568465848a2171799c5a2531cd74e0d84ca675
HectorStiven <vigoyahecto90@gmail.com>
2023-12-21 15:22:42 -0500

    servicio nuevo para previsualizar documentos enviadso de recuado

--- recaudo/views/tasa_retributiva_vertimiento_viwes.py
+++ recaudo/views/tasa_retributiva_vertimiento_viwes.py
@@ -1,7 +1,7 @@
 
 from gestion_documental.views.archivos_digitales_views import ArchivosDgitalesCreate
 from recaudo.models.tasa_retributiva_vertimiento_models import documento_formulario_recuado
-from recaudo.serializers.tasa_retributiva_vertimiento_serializers import documento_formulario_recuados_serializer
+from recaudo.serializers.tasa_retributiva_vertimiento_serializers import documento_formulario_recuados_Getserializer, documento_formulario_recuados_serializer
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework import generics,status
@@ -15,18 +15,21 @@ class CrearDocumentoFormularioRecuado(generics.CreateAPIView):
 
     def create(self, request, *args, **kwargs):
         try:
-            archivos = request.FILES.getlist('archivo')
+            respuesta_archivo={}
+            archivo = request.FILES.get('archivo')
 
-            if archivos:
+            if archivo:
                 id_archivos = []
-
-                for archivo in archivos:
                     # Procesar cada archivo y obtener el ID del archivo digital
-                    archivo_digital_id = self.archivos_dgitales_create.crear_archivo({'es_Doc_elec_archivo': False}, archivo).data['data']['id_archivo_digital']
-                    id_archivos.append(archivo_digital_id)
+                respuesta_archivo = self.archivos_dgitales_create.crear_archivo({'es_Doc_elec_archivo': False}, archivo)
+                #.data['data']['id_archivo_digital']
+                if respuesta_archivo.status_code != status.HTTP_201_CREATED :
+                     return respuesta_archivo
+                data_archivo = respuesta_archivo.data['data'] 
+                data_archivo_id=data_archivo['id_archivo_digital']
 
                 # Puedes manejar múltiples archivos de diferentes maneras, aquí solo se toma el primer archivo
-                request.data['id_archivo_sistema'] = id_archivos[0]
+                request.data['id_archivo_sistema'] = data_archivo_id
 
                 serializer = self.serializer_class(data=request.data)
                 serializer.is_valid(raise_exception=True)
@@ -39,4 +42,22 @@ class CrearDocumentoFormularioRecuado(generics.CreateAPIView):
         except ValidationError as e:
             raise ValidationError({'error': 'Error al crear el registro', 'detail': e.detail})
 
+# class DocumentoFormularioRecaudoGET(generics.ListAPIView):
+#     queryset = documento_formulario_recuado.objects.all()
+#     serializer_class = documento_formulario_recuados_Getserializer
+#     def get(self,request):
+#         instance=self.queryset
+#         serializer = self.serializer_class(instance,many=True)
+
+#         return Response({'success':True, 'detail':'se encontraron los siguientes registros', 'data': serializer.data}, status=status.HTTP_200_OK)
+
+class DocumentoFormularioRecaudoGET(generics.ListAPIView):
+    queryset = documento_formulario_recuado.objects.all()
+    serializer_class = documento_formulario_recuados_Getserializer
+
+    def get(self, request):
+        instance = self.get_queryset()
+        serializer = self.serializer_class(instance, many=True)
+
+        return Response({'success': True, 'detail': 'Se encontraron los siguientes registros', 'data': serializer.data}, status=status.HTTP_200_OK)
 
