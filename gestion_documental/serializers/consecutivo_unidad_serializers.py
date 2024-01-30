afa0602811070fb2bb8ffe5a024e051e0c39e4b0
richardhbp99 <richardbejarano29@gmail.com>
2024-01-30 11:46:21 -0500

    listado de consecutivos

--- gestion_documental/serializers/consecutivo_unidad_serializers.py
+++ gestion_documental/serializers/consecutivo_unidad_serializers.py
@@ -1,8 +1,106 @@
 from rest_framework import serializers
+from gestion_documental.models.ccd_models import CatalogosSeriesUnidad
 
-from gestion_documental.models.consecutivo_unidad_models import ConfigTipoConsecAgno
+from gestion_documental.models.consecutivo_unidad_models import ConfigTipoConsecAgno, Consecutivo
+from transversal.models.organigrama_models import UnidadesOrganizacionales
 
 class ConfigTipoConsecAgnoGetSerializer(serializers.ModelSerializer):
+    nombre_unidad = serializers.ReadOnlyField(source='id_unidad.nombre', default=None)
+    persona_configura = serializers.SerializerMethodField()
     class Meta:
         model = ConfigTipoConsecAgno
         fields = '__all__'
+
+    def get_persona_configura(self,obj):
+        nombre_completo_responsable = None
+        if obj.id_persona_config_implementacion:
+            nombre_completo_responsable = None
+            nombre_list = [obj.id_persona_config_implementacion.primer_nombre, obj.id_persona_config_implementacion.segundo_nombre,
+                            obj.id_persona_config_implementacion.primer_apellido, obj.id_persona_config_implementacion.segundo_apellido]
+            nombre_completo_responsable = ' '.join(item for item in nombre_list if item is not None)
+            nombre_completo_responsable = nombre_completo_responsable if nombre_completo_responsable != "" else None
+        return nombre_completo_responsable
+        
+class ConfigTipoConsecAgnoCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ConfigTipoConsecAgno
+        fields = '__all__'
+
+class ConfigTipoConsecAgnoPutConSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ConfigTipoConsecAgno
+        fields = '__all__'
+
+class UnidadesGetSerializer(serializers.ModelSerializer):
+    #nombre_unidad_org_actual_admin_series = serializers.ReadOnlyField(source='id_unidad_org_actual_admin_series.nombre', default=None)
+    #codigo_unidad_org_actual_admin_series = serializers.ReadOnlyField(source='id_unidad_org_actual_admin_series.codigo', default=None)
+    
+    class Meta:
+        model = UnidadesOrganizacionales
+        fields = ['id_unidad_organizacional','codigo','nombre']
+
+class ConsecutivoCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Consecutivo
+        fields = '__all__'
+
+
+class CatalogosSeriesUnidadGetSerializer(serializers.ModelSerializer):
+
+
+    id_serie_doc=serializers.ReadOnlyField(source='id_catalogo_serie.id_serie_doc.id_serie_doc',default=None)
+    cod_serie_doc=serializers.ReadOnlyField(source='id_catalogo_serie.id_serie_doc.codigo',default=None)
+    nombre_serie_doc=serializers.ReadOnlyField(source='id_catalogo_serie.id_serie_doc.nombre',default=None)
+    id_subserie_doc=serializers.ReadOnlyField(source='id_catalogo_serie.id_subserie_doc.id_subserie_doc',default=None)
+    cod_subserie_doc=serializers.ReadOnlyField(source='id_catalogo_serie.id_subserie_doc.codigo',default=None)
+    nombre_subserie_doc=serializers.ReadOnlyField(source='id_catalogo_serie.id_subserie_doc.nombre',default=None)
+    class Meta:
+        model = CatalogosSeriesUnidad
+        fields = ['id_cat_serie_und','id_serie_doc','cod_serie_doc','nombre_serie_doc','id_subserie_doc','cod_subserie_doc','nombre_subserie_doc']
+
+
+class ConsecutivoCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Consecutivo
+        fields = '__all__'
+
+class ConsecutivoGetSerializer(serializers.ModelSerializer):
+
+    consecutivo = serializers.SerializerMethodField()
+    nombre_unidad = serializers.ReadOnlyField(source='id_unidad.nombre', default=None)
+    id_serie_doc=serializers.ReadOnlyField(source='id_catalogo.id_catalogo_serie.id_serie_doc.id_serie_doc',default=None)
+    cod_serie_doc=serializers.ReadOnlyField(source='id_catalogo.id_catalogo_serie.id_serie_doc.codigo',default=None)
+    nombre_serie_doc=serializers.ReadOnlyField(source='id_catalogo.id_catalogo_serie.id_serie_doc.nombre',default=None)
+    id_subserie_doc=serializers.ReadOnlyField(source='id_catalogo.id_catalogo_serie.id_subserie_doc.id_subserie_doc',default=None)
+    cod_subserie_doc=serializers.ReadOnlyField(source='id_catalogo.id_catalogo_serie.id_subserie_doc.codigo',default=None)
+    nombre_subserie_doc=serializers.ReadOnlyField(source='id_catalogo.id_catalogo_serie.id_subserie_doc.nombre',default=None)
+
+
+    class Meta:
+        model = Consecutivo
+        fields = '__all__'
+
+
+
+
+    def get_consecutivo(self,obj):
+        conf = ConfigTipoConsecAgno.objects.filter(agno_consecutivo=obj.agno_consecutivo,id_unidad=obj.id_unidad).first()
+
+        numero_con_ceros = str(obj.nro_consecutivo).zfill(conf.cantidad_digitos)
+        cod_se_sub = ""
+
+        if obj.id_catalogo:
+            cod_serie = obj.id_catalogo.id_catalogo_serie.id_serie_doc.codigo
+            cod_se_sub = cod_serie
+            if obj.id_catalogo.id_catalogo_serie.id_subserie_doc:
+                cod_subserie =obj.id_catalogo.id_catalogo_serie.id_subserie_doc.codigo
+                cod_se_sub = cod_serie+'.'+cod_subserie
+        
+        if cod_se_sub != "":
+
+            conseg_nuevo = obj.id_unidad.codigo+'.'+cod_se_sub+'.'+str(obj.agno_consecutivo)[-2:]+'.'+numero_con_ceros
+        else:
+            conseg_nuevo = obj.id_unidad.codigo+'.'+str(obj.agno_consecutivo)[-2:]+'.'+numero_con_ceros
+        
+        return conseg_nuevo
+
