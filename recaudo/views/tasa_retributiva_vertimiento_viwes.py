644986591ef405a97b93e028f7cf053a6a632551
HectorStiven <vigoyahecto90@gmail.com>
2024-02-07 08:23:00 -0500

    ajustar sistema de nombres de tabñlas y servicios para cargar el documento

--- recaudo/views/tasa_retributiva_vertimiento_viwes.py
+++ recaudo/views/tasa_retributiva_vertimiento_viwes.py
@@ -1,11 +1,13 @@
 
 from gestion_documental.views.archivos_digitales_views import ArchivosDgitalesCreate
-from recaudo.models.tasa_retributiva_vertimiento_models import  CaptacionMensualAgua, T0444Formulario, documento_formulario_recuado
-from recaudo.serializers.tasa_retributiva_vertimiento_serializers import  T0444FormularioSerializer, documento_formulario_recuados_Getserializer, documento_formulario_recuados_serializer
+from recaudo.models.tasa_retributiva_vertimiento_models import  CaptacionMensualAgua, T0444Formulario, T458PrincipalLiquidacion, documento_formulario_recuado
+from recaudo.serializers.tasa_retributiva_vertimiento_serializers import  PrincipalLiquidacionSerializer, T0444FormularioSerializer, documento_formulario_recuados_Getserializer, documento_formulario_recuados_serializer
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework import generics,status
 from rest_framework.views import APIView
+import json
+
 
 from transversal.views.alertas_views import AlertaEventoInmediadoCreate
 
@@ -72,7 +74,38 @@ class DocumentoFormularioRecaudoGET(generics.ListAPIView):
 
 class T0444444FormularioView(APIView):
     def post(self, request, *args, **kwargs):
-        serializer = T0444FormularioSerializer(data=request.data)
+        json_informacionFuentesAbastecimiento=json.loads(request.data.get('informacionFuentesAbastecimiento'))
+        json_factoresUtilizacion= json.loads(request.data.get('factoresUtilizacion'))
+        json_captacionesMensualesAgua=json.loads(request.data.get('captacionesMensualesAgua'))
+        # print(type(json_captacionesMensualesAgua))
+        # print("###############")
+        # print(type(json_factoresUtilizacion))
+        # print("###############")
+
+        # print(type(json_informacionFuentesAbastecimiento))
+        # print(request.data)
+        data_completa=request.data.dict()
+        # print(type(data_completa))
+        data_completa.pop('captacionesMensualesAgua',None)
+        data_completa.pop('factoresUtilizacion',None)
+        data_completa.pop('informacionFuentesAbastecimiento',None)
+        data_completa.pop('id_archivo_digital', None)
+        data_completa["informacionFuentesAbastecimiento"]=json_informacionFuentesAbastecimiento
+        data_completa['factoresUtilizacion']=json_factoresUtilizacion
+        data_completa['captacionesMensualesAgua']=json_captacionesMensualesAgua 
+        vista_archivos = ArchivosDgitalesCreate()
+        archivo = request.FILES.get('id_archivo_sistema')
+        ruta = "home,BIA,Otros,Recaudo,AutoDeclaracion"
+        respuesta_archivo = vista_archivos.crear_archivo({"ruta":ruta,'es_Doc_elec_archivo':False},archivo)
+        if respuesta_archivo.status_code != status.HTTP_201_CREATED:
+            return respuesta_archivo
+        data_archivo = respuesta_archivo.data['data']
+
+        id_archivo =data_archivo['id_archivo_digital']
+        # print(data_archivo)
+        data_completa.pop('id_archivo_sistema', None)
+        data_completa['id_archivo_sistema']=id_archivo
+        serializer = T0444FormularioSerializer(data=data_completa)
 
         if serializer.is_valid():
             serializer.save()
@@ -121,3 +154,21 @@ class CaptacionMensualAguaViwes(generics.ListAPIView):
     def get(self, request, *args, **kwargs):
         MES_CHOICESs = dict(CaptacionMensualAgua.MES_CHOICES)
         return Response({'meses': MES_CHOICESs})
+    
+
+
+
+class T458PrincipalLiquidacionPorExpediente(generics.ListAPIView):
+    serializer_class = PrincipalLiquidacionSerializer
+
+    def get_queryset(self):
+        expediente = self.kwargs['expediente']
+        return T458PrincipalLiquidacion.objects.filter(T458expediente=expediente)
+
+    def get(self, request, *args, **kwargs):
+        try:
+            queryset = self.get_queryset()
+            serializer = self.serializer_class(queryset, many=True)
+            return Response({'success': True, 'detail': 'Se encontraron los siguientes registros', 'data': serializer.data}, status=status.HTTP_200_OK)
+        except T458PrincipalLiquidacion.DoesNotExist:
+            return Response({'success': False, 'detail': 'No se encontraron registros para el expediente proporcionado'}, status=status.HTTP_404_NOT_FOUND)
