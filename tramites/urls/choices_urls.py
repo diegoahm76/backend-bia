007475cb4ea23a4053fc7d68e21cf2d2ca1c9054
kevin <kevinandrescas98@gmail.com>
2024-02-13 21:48:41 -0500

    Update_Consulta_estados

--- tramites/urls/choices_urls.py
+++ tramites/urls/choices_urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('cod-tipo-operacion-tramite/', views.CodTipoOperacionTramiteChoices.as_view(), name='cod-tipo-operacion-tramite'),
     path('cod-calendario-habiles/', views.CodCalendarioHabilesChoices.as_view(), name='cod-calendario-habiles'),
     path('cod-tipo-permiso-ambiental/', views.CodTipoPermisoAmbientalChoices.as_view(), name='cod-tipo-permiso-ambiental'),
+    path('estado-solicitud-tramite/', views.EstadoSolicitudTramite.as_view(), name='estado-solicitud-tramite'),
 ]
