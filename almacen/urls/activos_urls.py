566021f836bd42d9cac5a1943575a3a6886d2fd9
kevin <kevinandrescas98@gmail.com>
2024-03-12 08:29:29 -0500

    Refactor: Cambios_Baja_Activos

--- almacen/urls/activos_urls.py
+++ almacen/urls/activos_urls.py
@@ -10,14 +10,9 @@ urlpatterns = [
     #Baja_Activos
     path('busqueda-avanzada-bienes/get/',views.BuscarBien.as_view(),name='busqueda-avanzada-bien'),
     path('crear-baja-activos/create/',views.RegistrarBajaCreateView.as_view(),name='crear-baja-activo'),
-
-
-
-
-
-
-
-
+    path('restablecer-consecutivo/get/',views.UltimoConsecutivoView.as_view(),name='consecutivo-baja-activo'),
+    path('actualizar-baja-activos/update/<int:pk>/', views.ActualizarBajaActivosView.as_view(), name='actualizar-baja-activo'),
+    path('eliminar-baja-activos/<int:pk>/', views.BorrarBajaActivosView.as_view(), name='borrar-baja-activos'),
 
 
 
