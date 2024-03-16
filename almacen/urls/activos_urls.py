0f0cabc0778038472afd7dda268cff45401765fd
kevin <kevinandrescas98@gmail.com>
2024-03-16 10:21:38 -0500

    Fix: Crud_Actualizacion_Baja_Activos

--- almacen/urls/activos_urls.py
+++ almacen/urls/activos_urls.py
@@ -15,13 +15,21 @@ urlpatterns = [
     path('eliminar-baja-activos/<int:pk>/', views.BorrarBajaActivosView.as_view(), name='borrar-baja-activos'),
     path('info-baja-activos/get/<int:consecutivo>/', views.BajaActivosPorConsecutivo.as_view(), name='baja-activos-consecutivo'),
     path('bajas-activos/get/', views.ListarBajasActivosView.as_view(), name='listar_bajas_activos'),
+    path('crear-anexo-opcional/create/<int:id_baja_activo>/',views.CrearArchivosOpcionales.as_view(),name='crear-archivos-opcionales'),
+    path('actualizar-anexo-opcional/update/<int:id_baja_activo>/',views.ActualizarAnexoOpcional.as_view(),name='actualizar-archivos-opcionales'),
+    path('eliminar-anexo-opcional/delete/<int:id_baja_activo>/',views.EliminarAnexoOpcional.as_view(),name='actualizar-archivos-opcionales'),
+    path('listar-anexo-opcional/get/<int:id_baja_activo>/',views.ListarAnexoOpcional.as_view(),name='actualizar-archivos-opcionales'),
+
 
     #Solicitud_Bajas_Activos
     path('crear-solicitud-baja-activos/create/',views.CrearSolicitudActivosView.as_view(),name='crear-solicitud-baja-activo'),
     path('editar-solicitud-activos/<int:pk>/', views.EditarSolicitudActivosView.as_view(), name='editar-solicitud-activos'),
     path('listar-unidades-medida/get/',views.ListarUnidadesMedidaActivas.as_view(),name='listar-unidades-medida'),
-    path('detalle-solicitud-activos/<int:id_solicitud_viaje>/', views.DetalleSolicitudActivosView.as_view(), name='detalle-solicitud-activos'),
-    path('resumen-solicitud-activos/<int:id_solicitud_viaje>/', views.ResumenSolicitudGeneralActivosView.as_view(), name='resumen-solicitud-activos'),
+    path('detalle-solicitud-activos/<int:id_solicitud_activo>/', views.DetalleSolicitudActivosView.as_view(), name='detalle-solicitud-activos'),
+    path('resumen-solicitud-activos/<int:id_solicitud_activo>/', views.ResumenSolicitudGeneralActivosView.as_view(), name='resumen-solicitud-activos'),
+    path('resumen-solicitud-activos/<int:id_solicitud_activo>/', views.ResumenSolicitudGeneralActivosView.as_view(), name='resumen-solicitud-activos'),
+    path('cancelar-solicitud-activos/<int:id_solicitud_activo>/', views.CancelarSolicitudActivos.as_view(), name='cancelar-solicitud-activos'),
+    path('busqueda-solicitudes-realizadas/get/', views.BusquedaAvanzadaSolicitudesActivos.as_view(), name='busqueda-solicitud-activos'),
 
 
 
