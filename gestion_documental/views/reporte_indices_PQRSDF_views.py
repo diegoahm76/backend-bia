cd301278b764887b84437ba4d2fdd6e65e39f729
richardhbp99 <richardbejarano29@gmail.com>
2024-03-18 19:06:00 -0500

    fix:pqrsdf vencidas

--- gestion_documental/views/reporte_indices_PQRSDF_views.py
+++ gestion_documental/views/reporte_indices_PQRSDF_views.py
@@ -13,9 +13,10 @@ from rest_framework.exceptions import ValidationError,NotFound,PermissionDenied
 import os
 from rest_framework.permissions import IsAuthenticated
 from datetime import datetime, timedelta, timezone
-from gestion_documental.models.radicados_models import PQRSDF, EstadosSolicitudes
+from gestion_documental.models.radicados_models import PQRSDF, AsignacionPQR, EstadosSolicitudes
 from gestion_documental.serializers.reporte_indices_archivos_carpetas_serializers import ReporteIndicesTodosGetSerializer,SucursalesEmpresasSerializer,UnidadesGetSerializer,CatalogosSeriesUnidadGetSerializer
 from transversal.models.entidades_models import SucursalesEmpresas
+from transversal.models.organigrama_models import Organigramas, UnidadesOrganizacionales
 
 
 
@@ -50,7 +51,7 @@ class ReporteIndicesTodosGet(generics.ListAPIView):
         en_gestion = instance.filter(id_estado_actual_solicitud=5).count()
         resueltas = instance.filter(id_estado_actual_solicitud=14).count()
 
-        vencidas = instance.filter(id_radicado__isnull=False)
+        vencidas = instance.filter(id_radicado__isnull=False).exclude(id_estado_actual_solicitud=14)
         today = datetime.now()
         contador = 0
 
@@ -59,10 +60,10 @@ class ReporteIndicesTodosGet(generics.ListAPIView):
             if today > v.fecha_radicado + timedelta(days=v.dias_para_respuesta):
                     contador += 1
 
-        print(contador)
-        print(radicadas)
-        print(en_gestion)
-        print(resueltas)
+        # print(contador)
+        # print(radicadas)
+        # print(en_gestion)
+        # print(resueltas)
 
         series = []
 
@@ -146,6 +147,22 @@ class ReporteIndicesSucursalesGet(generics.ListAPIView):
                 nombre = nombre_estado.nombre
             data.append({'name':nombre,'data':matriz_transpuesta[x]})
 
+        
+        
+       
+        today = datetime.now()
+        contador = 0
+        conte_vencidos =[]
+        for s in sucursales:
+            pqrsdfs = instance.filter(id_sucursal_especifica_implicada=s).exclude(id_estado_actual_solicitud=14)
+            contador = 0
+            for v in pqrsdfs:
+
+                if v.dias_para_respuesta and  today > v.fecha_radicado + timedelta(days=v.dias_para_respuesta):
+                    contador += 1
+            conte_vencidos.append(contador)
+
+        data.append({'name':'Vencidos', 'data':conte_vencidos})
         return Response({'success':True,'detail':'Se encontraron los siguientes registros.','data':{'series':data, "categories":categories}},status=status.HTTP_200_OK)
         
 class ReporteIndicesSucursalesTiposPQRSDFGet(generics.ListAPIView):
@@ -233,9 +250,7 @@ class ReporteIndicesTiposSucursalesPQRSDFGet(generics.ListAPIView):
              
         instance = self.get_queryset().filter(**filter)
         sucursales =SucursalesEmpresas.objects.all()
-        #1 RADICADO
-        #2 EN GESTION
-        #14 RESUELTA
+
         
         conteo_sucursales = []
         categories = []
@@ -262,3 +277,83 @@ class ReporteIndicesTiposSucursalesPQRSDFGet(generics.ListAPIView):
 
         return Response({'success':True,'detail':'Se encontraron los siguientes registros.','data':{'series':data, "categories":categories}},status=status.HTTP_200_OK)
         
+
+
+
+
+
+class ReporteIndicesUnidadPQRSDFGet(generics.ListAPIView):
+    serializer_class = None
+    queryset = PQRSDF.objects.all()
+    permission_classes = [IsAuthenticated]
+    
+    def get(self,request):
+        #2 RADICADO
+        #5 EN GESTION
+        #14 RESUELTA
+        estados = [2,5,14]
+
+        filter={}
+        fecha_inicio = None
+        fecha_fin = None
+        for key, value in request.query_params.items():
+
+            if key == 'fecha_inicio':
+                if value != '':
+                    
+                    filter['fecha_registro__gte'] = datetime.strptime(value, '%Y-%m-%d').date()
+                    fecha_inicio = datetime.strptime(value, '%Y-%m-%d').date()
+            if key == 'fecha_fin':
+                if value != '':
+                    fecha_fin = datetime.strptime(value, '%Y-%m-%d').date()
+                    filter['fecha_registro__lte'] = datetime.strptime(value, '%Y-%m-%d').date()
+             
+        instance = self.get_queryset().filter(**filter)
+        
+        organigrama = Organigramas.objects.filter(actual=True)
+        if not organigrama:
+            raise NotFound('No existe ningún organigrama activado')
+        if len(organigrama) > 1:
+            raise PermissionDenied('Existe más de un organigrama actual, contacte a soporte')
+        
+        organigrama_actual = organigrama.first()
+        unidades_organigrama_actual = UnidadesOrganizacionales.objects.filter(id_organigrama=organigrama_actual.id_organigrama)
+        asignaciones = AsignacionPQR.objects.filter(id_und_org_seccion_asignada__in=unidades_organigrama_actual,cod_estado_asignacion='Ac')
+
+        conteo_unidades = []
+        categories  = []
+        for unidad in unidades_organigrama_actual:
+            array =[]
+            for estado in estados:
+                conteo = asignaciones.filter(id_und_org_seccion_asignada=unidad, cod_estado_asignacion='Ac', id_pqrsdf__id_estado_actual_solicitud=estado).count()
+                array.append(conteo)
+
+            conteo_unidades.append(array)
+            categories.append(unidad.nombre)
+        matriz_transpuesta = [[fila[i] for fila in conteo_unidades] for i in range(len(conteo_unidades[0]))]
+        print(matriz_transpuesta)
+
+        data=[]
+        data.append({'name':'Radicados', 'data':matriz_transpuesta[0]})
+        data.append({'name':'En Gestión', 'data':matriz_transpuesta[1]})
+        data.append({'name':'Resuelta', 'data':matriz_transpuesta[2]})
+
+
+        today = datetime.now()
+        contador = 0
+        conte_vencidos =[]
+        for s in unidades_organigrama_actual:
+            
+            contador = 0
+            asignacion_unidad = asignaciones.filter(id_und_org_seccion_asignada=s)
+            for v in asignacion_unidad:
+
+                if v.id_pqrsdf.dias_para_respuesta and  today > v.id_pqrsdf.fecha_radicado + timedelta(days=v.id_pqrsdf.dias_para_respuesta):
+                    contador += 1
+            conte_vencidos.append(contador)
+
+        data.append({'name':'Vencidos', 'data':conte_vencidos})
+
+
+        return Response({'success':True,'detail':'Se encontraron los siguientes registros.','data':{'series':data, "categories":categories}},status=status.HTTP_200_OK)
+     
