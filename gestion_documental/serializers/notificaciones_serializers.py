7c6afcdd9f046acd4ab91acd09ac4ca5430ffb15
EstibenSanchez <brayan.barragan@unillanos.edu.co>
2024-05-20 14:10:31 -0500

    FIX: Tipo notificacion

--- gestion_documental/serializers/notificaciones_serializers.py
+++ gestion_documental/serializers/notificaciones_serializers.py
@@ -39,7 +39,7 @@ class NotificacionesCorrespondenciaSerializer(serializers.ModelSerializer):
     funcuinario_solicitante = serializers.SerializerMethodField()
     unidad_solicitante = serializers.CharField(source='id_und_org_oficina_solicita.nombre')
     estado_notificacion = estado_solicitud = serializers.CharField(source='get_cod_estado_display',read_only=True,default=None)
-    tipo_notificacion = serializers.SerializerMethodField()
+   # tipo_notificacion = serializers.SerializerMethodField()
 
 
     class Meta:
