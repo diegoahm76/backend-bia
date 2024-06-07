825572c391fca3ec43ac792c96ae298d2a53e3fa
EstibenSanchez <brayan.barragan@unillanos.edu.co>
2024-06-07 06:52:13 -0500

    FIX: Actualizar documentos

--- gestion_documental/urls/trd_urls.py
+++ gestion_documental/urls/trd_urls.py
@@ -70,6 +70,8 @@ urlpatterns = [
     path('codigo-verificacion-validacion/', views.ValidacionCodigoView.as_view(), name='codigo-verificacion-validacion'),
     path('documentos-finalizados-get/', views.DocumentosFinalizadosList.as_view(), name='documentos-finalizados-get'),
     path('crear-documento-cargado/', views.SubirDocumentoAlGenerador.as_view(), name='crear-documento-cargado'),
+    path('actualizar-documentos/', views.ActualizarDocumentos.as_view(), name='actualizar-documentos'),
+
 
  
 ]
