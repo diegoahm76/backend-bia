1d32b568862c91823afbc6c9cea9a118319f7b43
Yeison Piñeros <yeisonfabian_10@hotmail.com>
2024-06-25 19:16:42 -0500

    Cambio de modelos de rubros

--- seguimiento_planes/serializers/planes_serializer.py
+++ seguimiento_planes/serializers/planes_serializer.py
@@ -104,9 +104,16 @@ class TipoSerializer(serializers.ModelSerializer):
         model = Tipo
         fields = '__all__'
 
-class RubroSerializer(serializers.ModelSerializer):   
-    cod_presupuestal = serializers.ReadOnlyField(source='id_rubro_parametrica.cod_pre', default=None) 
-    cuenta = serializers.ReadOnlyField(source='id_rubro_parametrica.cuenta', default=None)
+
+class RubroSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Rubro
+        fields = '__all__'
+
+class RubroSerializerGet(serializers.ModelSerializer):   
+    cod_presupuestal = serializers.ReadOnlyField(source='id_rubro.cod_pre', default=None) 
+    cuenta = serializers.ReadOnlyField(source='id_rubro.cuenta', default=None)
     nombre_fuente = serializers.ReadOnlyField(source='id_fuente.nombre_fuente', default=None)
 
     class Meta:
